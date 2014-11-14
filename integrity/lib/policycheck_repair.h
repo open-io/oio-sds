73b8054dad3f51e958fb0476b0e727041148dff8
AMO <amogrid@gmail.com>
2014-11-14 15:50:07 +0100

    integrity/policycheck: add the following flags to policycheck:
    NOMD5: skip chunk md5 testing (so no data gets downloaded)
    FORCE_RAWX_REBUILD: consider unreachable rawx as broken
    
    Change-Id: I613a04134a03a101f79bf661e6156aec22caf92e

--- integrity/lib/policycheck_repair.h
+++ integrity/lib/policycheck_repair.h
@@ -10,13 +10,20 @@ struct grid_lbpool_s;
 struct namespace_info_s;
 struct m2v2_check_s;
 
+/** Activate check only (aka dryrun) mode. */
+#define POLCHK_FLAG_CHECKONLY			0x01
+/** Skip check for chunk md5. */
+#define POLCHK_FLAG_NOMD5				0x02
+/** Consider unreachable rawx as broken. */
+#define POLCHK_FLAG_FORCE_RAWX_REBUILD	0x04
+
 struct policy_check_s
 {
 	struct hc_url_s *url;
 	struct hc_resolver_s *resolver;
 	struct grid_lbpool_s *lbpool;
 	struct namespace_info_s *nsinfo;
-	gboolean check_only;
+	guint32 flags;
 
 	struct m2v2_check_s *check;
 	gchar **m2urlv;
@@ -41,17 +48,25 @@ GError* policy_load_beans(struct policy_check_s *pc);
  * policy_load_beans() and policy_check_and_repair() yourself.
  *
  * @param url URL to the content
- * @param check_only TRUE to check but not try to repair
+ * @param flags check flags (set to 0 if not needed)
  * @param err A place where to put errors
  * @return The number of problems detected on the content
  */
 gint check_and_repair_content(struct hc_url_s *url,
-		gboolean check_only, GError **error);
+		guint32 flags, GError **error);
 
 /**
  * Same as check_and_repair_content() with a different signature.
  */
 gint check_and_repair_content2(const gchar *ns,
+		const gchar *container_id, const gchar *content_name,
+		const gchar *content_version, guint32 flags, GError **error);
+
+/**
+ * Same as check_and_repair_content2() with a different signature and
+ * flags forced to POLCHK_FLAG_FORCE_RAWX_REBUILD and POLCHK_FLAG_NOMD5.
+ */
+gint rebuild_broken_chunks_for_content(const gchar *ns,
 		const gchar *container_id, const gchar *content_name,
 		const gchar *content_version, gboolean check_only, GError **error);
 
