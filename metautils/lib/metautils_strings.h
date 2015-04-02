32a47025ff6c82e870bb0b7b143c4265096148ea
Jean-Francois SMIGIELSKI <jf.smigielski@gmail.com>
2015-04-02 10:11:33 +0200

    metautils: useless/void comments removed.

--- metautils/lib/metautils_strings.h
+++ metautils/lib/metautils_strings.h
@@ -62,102 +62,36 @@ void metautils_str_lower(register gchar *s);
 /* Returns FALSE if 's' is not 'slen' long and contains a non-hexa character. */
 gboolean metautils_str_ishexa(const gchar *s, gsize slen);
 
-/**
- * @param s
- * @param l
- * @return
- */
 gsize strlen_len(const guint8 * s, gsize l);
 
-/**
- * Convert an hexa string to its binary form
- *
- * @param src the hexa string to convert
- * @param dst the allocated destination of the binary form
- * @param dst_size
- * @param error
- *
- * @return TRUE or FALSE if an error occured (error is set);
- */
+/** Convert an hexa string to its binary form */
 gboolean hex2bin(const gchar * src, void * dst, gsize dst_size, GError** error);
 
-/**
- * Fills d (which size is dS) with the hexadecimal alpha-numerical representation
- * of the content of s (which size is sS)
- *
- * @param s
- * @param sS
- * @param d
- * @param dS
- */
+/** Fills d (which size is dS) with the hexadecimal alpha-numerical
+ * representation of the content of s (which size is sS) */
 void buffer2str(const void *s, size_t sS, char *d, size_t dS);
 
-/**
- * Splits the given buffer (considered as a non NULL-terminated) into 
- * newly allocated tokens (wrapping g_strsplit())
- *
- * @param buf
- * @param buflen
- * @param separator
- * @param max_tokens
- * @return
- */
+/** Splits the given buffer (considered as a non NULL-terminated) into 
+ * newly allocated tokens (wrapping g_strsplit()) */
 gchar **buffer_split(const void *buf, gsize buflen, const gchar * separator, gint max_tokens);
 
-/**
- * Check a segment of data is filled with 0
- *
- * @param data the segment of data to check
- * @param data_size the size of the segment to check (in bytes)
- * @return TRUE if the segment is filled with 0, FALSE otherwise
- */
+/** Check a segment of data is filled with 0 */
 gboolean data_is_zeroed(const void *data, gsize data_size);
 
-/**
- * @param start
- * @param end
- * @return
- */
 gchar** metautils_decode_lines(const gchar *start, const gchar *end);
 
-/**
- * @param strv
- * @return
- */
 GByteArray* metautils_encode_lines(gchar **strv);
 
-/**
- * @param src
- * @param src_size
- * @param dst
- * @param dst_size
- * @param dst_bitlength
- * @return the size of the string written, not including the trailing '\0'
- */
 gsize metautils_hash_content_path(const gchar *src, gsize src_size,
 	gchar *dst, gsize dst_size, gsize dst_bitlength);
 
-/**
- * Fills the hash_path argument with a hash of the given file_name.
+/** Fills the hash_path argument with a hash of the given file_name.
  * The hash is a sequence of hash_depth subdirectories, whose each
- * directory's name is hash_size long.
- *
- * @param file_name
- * @param hash_depth
- * @param hash_size
- * @param hash_path
- */
+ * directory's name is hash_size long. */
 void build_hash_path(const char *file_name, int hash_depth, int hash_size,
 		char **hash_path);
 
-/**
- * Calls g_strcmp0(a,b) and ignores its third argument.
- * @see g_strcmp0() from the GLib2
- * @param a
- * @param b
- * @param ignored
- * @return
- */
+/** Calls g_strcmp0(a,b) and ignores its third argument. */
 int metautils_strcmp3(gconstpointer a, gconstpointer b, gpointer ignored);
 
 /** Returns the boolean value of the textual and human readable boolean
