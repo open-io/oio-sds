514deb3af03104bc54a306f70f84862d68626ada
Jean-Francois SMIGIELSKI <jf.smigielski@gmail.com>
2015-06-28 21:33:55 +0200

    sdk: URI-encoding management.

--- sdk/test_roundtrip.c
+++ sdk/test_roundtrip.c
@@ -16,6 +16,10 @@ You should have received a copy of the GNU Lesser General Public
 License along with this library.
 */
 
+#ifndef G_LOG_DOMAIN
+#define G_LOG_DOMAIN "oio.sdk.test"
+#endif
+
 #include <string.h>
 #include <unistd.h>
 
@@ -66,24 +70,27 @@ main(int argc, char **argv)
 	struct oio_error_s *err = NULL;
 	int has = 0;
 
-	g_snprintf (tmppath, sizeof(tmppath), "/tmp/test-roundtrip-%d-%lu-", getpid(), time(0));
+	g_snprintf (tmppath, sizeof(tmppath), "/tmp/test-roundtrip-%d-%lu-",
+			getpid(), time(0));
 	size_t tmplen = strlen(tmppath);
 	_randomize_string (tmppath+tmplen, random_chars, MIN(16,sizeof(tmppath)-tmplen));
 	_randomize_string (tmpblob, random_chars, sizeof(tmpblob));
 
-
-	url = hc_url_oldinit(str_url);
+	url = hc_url_init(str_url);
 	if (!url) {
 		g_printerr ("Invalid URL [%s]\n", str_url);
 		return 2;
 	}
-	if (!hc_url_has (url, HCURL_PATH)) {
-		hc_url_set (url, HCURL_PATH, tmpblob);
-	}
 	if (!hc_url_has_fq_path(url)) {
-		g_printerr ("Partial URL [%s]\n", str_url);
+		g_printerr ("Partial URL [%s]: %s requires a NS (%s), an ACCOUNT (%s),"
+				" an USER (%s) and a PATH (%s)\n", str_url, argv[0],
+				hc_url_has (url, HCURL_NS)?"ok":"missing",
+				hc_url_has (url, HCURL_ACCOUNT)?"ok":"missing",
+				hc_url_has (url, HCURL_USER)?"ok":"missing",
+				hc_url_has (url, HCURL_PATH)?"ok":"missing");
 		return 3;
 	}
+	GRID_INFO("URL valid [%s]", hc_url_get (url, HCURL_WHOLE));
 
 	/* Initiate a client */
 	err = oio_sds_init (&client, hc_url_get(url, HCURL_NS));
@@ -92,6 +99,7 @@ main(int argc, char **argv)
 				oio_error_message(err));
 		return 4;
 	}
+	GRID_INFO("Client ready to [%s]", hc_url_get (url, HCURL_NS));
 
 	/* Check the presence of the targetd content */
 	err = oio_sds_has (client, url, &has);
@@ -104,6 +112,7 @@ main(int argc, char **argv)
 		g_printerr ("File already present\n");
 		return 6;
 	}
+	GRID_INFO("Content absent as expected");
 
 	/* Ok, the content was absent so we can upload it */
 	err = oio_sds_upload_from_file (client, url, str_path);
@@ -112,6 +121,7 @@ main(int argc, char **argv)
 				oio_error_message(err));
 		return 7;
 	}
+	GRID_INFO("Content uploaded");
 
 	/*the upload succeeded, so the presence check should succeed */
 	has = 0;
@@ -125,6 +135,7 @@ main(int argc, char **argv)
 		g_printerr ("File not present\n");
 		return 9;
 	}
+	GRID_INFO("Content present as expected");
 
 	/* and it is also possible to download the file */
 	err = oio_sds_download_to_file (client, url, tmppath);
@@ -133,6 +144,7 @@ main(int argc, char **argv)
 				oio_error_message(err));
 		return 10;
 	}
+	GRID_INFO("Content downloaded");
 
 	/* and we let the container clean, we remove the blob in the container. */
 	err = oio_sds_delete (client, url);
@@ -141,6 +153,7 @@ main(int argc, char **argv)
 				oio_error_message(err));
 		return 11;
 	}
+	GRID_INFO("Content removed");
 
 	g_remove (tmppath);
 	hc_url_pclean (&url);
