76ce5d301d6d7ef5ed80d9b2bbe70856678143e3
Julien Kasarherou <julien.kasarherou@openio.io>
2015-07-31 11:59:52 +0200

    proxy: fix domain logging

--- proxy/common.c
+++ proxy/common.c
@@ -16,7 +16,12 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#ifndef G_LOG_DOMAIN
+#define G_LOG_DOMAIN "proxy"
+#endif
+
 #include "common.h"
+
 
 gboolean
 validate_namespace (const gchar * ns)
