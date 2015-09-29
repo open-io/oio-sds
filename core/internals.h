50ce538daff9f51cf63c211bc6f807a4a97f0253
Jean-Francois SMIGIELSKI <jf.smigielski@gmail.com>
2015-09-29 09:50:38 +0200

    core: autocontainer example + link proposal

--- core/internals.h
+++ core/internals.h
@@ -1,7 +1,4 @@
 #ifndef OIO_SDS__core__internals_h
 # define OIO_SDS__core__internals_h 1
 
-const char * oio_str_autocontainer_hash (const guint8 *bin, gsize len,
-		gchar *dst, const struct oio_str_autocontainer_config_s *cfg);
-
 #endif /*OIO_SDS__core__internals_h*/
