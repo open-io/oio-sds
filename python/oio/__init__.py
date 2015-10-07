1a0b1a18e99635fcb60a7d2c0279203576488074
Julien Kasarherou <julien@openio.io>
2015-10-07 15:08:28 +0200

    remove pbr runtime dependency

--- python/oio/__init__.py
+++ python/oio/__init__.py
@@ -1,4 +1,10 @@
-import pbr.version
-_version_info = pbr.version.VersionInfo('oio')
-__version__ = _version_info.release_string()
-__canonical_version = _version_info.version_string()
+import pkg_resources
+
+try:
+    __version__ = __canonical_version__ = pkg_resources.get_provider(
+        pkg_resources.Requirement.parse('oio')).version
+except pkg_resources.DistributionNotFound:
+    import pbr.version
+    _version_info = pbr.version.VersionInfo('oio')
+    __version__ = _version_info.release_string()
+    __canonical_version = _version_info.version_string()
