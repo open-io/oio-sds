eb42dd44bc07dad14df6d84fd40536a2feef83f4
Narthorn <narthorn@gmail.com>
2015-11-03 11:41:48 +0100

    cache: add memcached cache backend

---
+++ cache/cache_memcached.h
@@ -0,0 +1,28 @@
+/*
+OpenIO SDS cache
+Copyright (C) 2015 OpenIO, original work as part of OpenIO Software Defined Storage
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3.0 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library.
+*/
+
+#ifndef OIO_SDS__cache__cache_memcached_h
+# define OIO_SDS__cache__cache_memcached_h 1
+
+#include "cache.h"
+
+struct oio_cache_s * oio_cache_make_memcached (const char *ip, int port);
+
+struct oio_cache_s * oio_cache_make_memcached_config (const char *config);
+
+#endif /*OIO_SDS__cache__cache_memcached_h*/
