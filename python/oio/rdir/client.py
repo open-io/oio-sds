d3dd528ad1b10ea51a47df81761ab69e5bc2505a
Christophe CARRE <christophe.carre@ymail.com>
2015-11-23 16:13:38 +0100

    rdir: create and link a reference to rdir service if necessary

--- python/oio/rdir/client.py
+++ python/oio/rdir/client.py
@@ -1,5 +1,5 @@
 from oio.common.client import Client
-from oio.common.exceptions import ClientException
+from oio.common.exceptions import ClientException, NotFound
 from oio.common.utils import true_value
 from oio.directory.client import DirectoryClient
 
@@ -10,8 +10,18 @@ class RdirClient(Client):
         self.autocreate = true_value(conf.get('autocreate', True))
         self.directory_client = DirectoryClient(conf)
 
+    # TODO keep rdir addr in local cache to avoid lookup requests
     def _get_rdir_addr(self, volume_id):
-        resp = self.directory_client.show(acct="_RDIR", ref=volume_id)
+        try:
+            resp = self.directory_client.show(acct='_RDIR', ref=volume_id)
+        except NotFound as e:
+            if self.autocreate:
+                self.directory_client.create('_RDIR', volume_id)
+                self.directory_client.link('_RDIR', volume_id, 'rdir')
+                resp = self.directory_client.show(acct='_RDIR', ref=volume_id)
+            else:
+                raise e
+
         for srv in resp['srv']:
             if srv['type'] == 'rdir':
                 return srv['host']
@@ -23,9 +33,13 @@ class RdirClient(Client):
             rdir_host, self.ns, action, volume_id)
         return uri
 
+    def _rdir_request(self, volume, method, action, **kwargs):
+        uri = self._make_uri(action, volume)
+        resp, body = self._direct_request(method, uri, **kwargs)
+        return resp, body
+
     def chunk_push(self, volume, container, content, chunk,
                    mtime=None, rtime=None):
-        uri = self._make_uri('rdir/push', volume)
         body = {'container': container,
                 'content': content,
                 'chunk': chunk}
@@ -34,27 +48,25 @@ class RdirClient(Client):
         if rtime:
             body['rtime'] = rtime
         headers = {}
-        if self.autocreate:
-            headers['x-oio-action-mode'] = 'autocreate'
 
-        self._direct_request('POST', uri, json=body, headers=headers)
+        self._rdir_request(volume, 'POST', 'rdir/push',
+                           json=body, headers=headers)
 
     def chunk_delete(self, volume, container, content, chunk):
-        uri = self._make_uri('rdir/delete', volume)
         body = {'container': container,
                 'content': content,
                 'chunk': chunk}
 
-        self._direct_request('DELETE', uri, json=body)
+        self._rdir_request(volume, 'DELETE', 'rdir/delete', json=body)
 
     def chunk_fetch(self, volume, limit=100, rebuild=False):
-        uri = self._make_uri('rdir/fetch', volume)
         req_body = {'limit': limit}
         if rebuild:
             req_body['rebuild'] = True
 
         while True:
-            resp, resp_body = self._direct_request('POST', uri, json=req_body)
+            resp, resp_body = self._rdir_request(volume, 'POST', 'rdir/fetch',
+                                                 json=req_body)
             resp.raise_for_status()
             if len(resp_body) == 0:
                 break
@@ -64,30 +76,23 @@ class RdirClient(Client):
             req_body['start_after'] = key
 
     def admin_broken_set(self, volume, date):
-        uri = self._make_uri('rdir/admin/broken', volume)
         body = {'date': date}
-
-        self._direct_request('POST', uri, json=body)
+        self._rdir_request(volume, 'POST', 'rdir/admin/broken', json=body)
 
     def admin_broken_get(self, volume):
-        uri = self._make_uri('rdir/admin/broken', volume)
-
-        resp, resp_body = self._direct_request('GET', uri)
+        resp, resp_body = self._rdir_request(volume, 'GET',
+                                             'rdir/admin/broken')
         return resp_body.get('date')
 
     def admin_lock(self, volume, who):
         uri = self._make_uri('rdir/admin/lock', volume)
         body = {'who': who}
 
-        self._direct_request('POST', uri, json=body)
+        self._rdir_request(volume, 'POST', 'rdir/admin/lock', json=body)
 
     def admin_unlock(self, volume):
-        uri = self._make_uri('rdir/admin/unlock', volume)
-
-        self._direct_request('POST', uri)
+        self._rdir_request(volume, 'POST', 'rdir/admin/unlock')
 
     def admin_show(self, volume):
-        uri = self._make_uri('rdir/admin/show', volume)
-
-        resp, resp_body = self._direct_request('GET', uri)
+        resp, resp_body = self._rdir_request(volume, 'GET', 'rdir/admin/show')
         return resp_body
