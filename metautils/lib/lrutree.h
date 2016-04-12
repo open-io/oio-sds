83d2f52da36fb8ab51de29e133ec96d4ae7440a6
Jean-Francois Smigielski <jf.smigielski@gmail.com>
2016-04-12 14:36:02 +0200

    metautils/lru: integrates timestamps to lru entries
    The time management is now manageable internally, and no wrappers are
    still necessary.

--- metautils/lib/lrutree.h
+++ metautils/lib/lrutree.h
@@ -37,104 +37,40 @@ struct lru_tree_s;
 struct lru_tree_s* lru_tree_create(GCompareFunc compare,
 		GDestroyNotify kfree, GDestroyNotify vfree, guint32 options);
 
-/**
- * Destroys the LRU-Tree and calls the liberation hook for each stored
- * pair.
- *
- * @param lt may be NULL
- */
+/* Destroys the LRU-Tree and calls the liberation hook for each stored
+   pair. */
 void lru_tree_destroy(struct lru_tree_s *lt);
 
-/**
- * Associates 'k' with 'v' in this container.
- *
- * @param lt not NULL
- * @param k not NULL
- * @param v noy NULL
- */
+guint lru_tree_remove_older (struct lru_tree_s *lt, gint64 oldest);
+
+guint lru_tree_remove_exceeding (struct lru_tree_s *lt, guint count);
+
 void lru_tree_insert(struct lru_tree_s *lt, gpointer k, gpointer v);
 
-/**
- * @param lt not NULL
- * @param k not NULL
- * @return the value associated to 'k'
- */
 gpointer lru_tree_get(struct lru_tree_s *lt, gconstpointer k);
 
-/**
- * @param lt not NULL
- * @param k not NULL
- * @return FALSE if removed or something else if the v has been removed.
- */
+/* Returns TRUE if the item keyed with 'k' has been removed. */
 gboolean lru_tree_remove(struct lru_tree_s *lt, gconstpointer k);
 
-/**
- * @param lt not NULL
- * @param k not NULL
- * @return NULL if not found or the 'v' associated to k
- */
+/* Returns NULL if not found or the item associated to 'k' */
 gpointer lru_tree_steal(struct lru_tree_s *lt, gconstpointer k);
 
-/**
- * @param lt not NULL
- * @param pk not NULL
- * @param pv not NULL
- * @return TRUE is pk and pv set
- */
 gboolean lru_tree_get_first(struct lru_tree_s *lt, gpointer *pk,
 		gpointer *pv);
 
-/**
- * @param lt not NULL
- * @param pk not NULL
- * @param pv not NULL
- * @return TRUE is pk and pv set
- */
 gboolean lru_tree_steal_first(struct lru_tree_s *lt, gpointer *pk,
 		gpointer *pv);
 
-/**
- * @param lt not NULL
- * @param pk not NULL
- * @param pv not NULL
- * @return TRUE is pk and pv set
- */
 gboolean lru_tree_get_last(struct lru_tree_s *lt, gpointer *pk,
 		gpointer *pv);
 
-/**
- * @param lt not NULL
- * @param pk not NULL
- * @param pv not NULL
- * @return TRUE is pk and pv set
- */
 gboolean lru_tree_steal_last(struct lru_tree_s *lt, gpointer *pk,
 		gpointer *pv);
 
-/**
- * Run the elements according to the TREE order, i.e. the order got from the
- * comparison function.
- *
- * @param lt not NULL
- * @param h a not NULL hook to be called on each tree element
- * @param hdata a (maybe NULL) arbitrary argument passed to each hook call.
- */
 void lru_tree_foreach_TREE(struct lru_tree_s *lt, GTraverseFunc h, gpointer hdata);
 
-/**
- * Run the elementsaccording to their last access order. The last element
- * will be the LRU elements.
- *
- * @see lru_tree_foreach()
- * @param h a not NULL hook to be called on each 2queue element
- * @param hdata a (maybe NULL) arbitrary argument passed to each hook call.
- */
 void lru_tree_foreach_DEQ(struct lru_tree_s *lt, GTraverseFunc h, gpointer hdata);
 
-/**
- * @param lt not NULL
- * @return the number of elements in lt
- */
 gint64 lru_tree_count(struct lru_tree_s *lt);
 
 #endif /*OIO_SDS__metautils__lib__lrutree_h*/
