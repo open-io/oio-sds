1962ca428bb0404f10af9eb40770ec37748bee64
Julien Kasarherou <julien.kasarherou@openio.io>
2016-05-11 17:25:13 +0200

    fixes for event-agent (#580)
    * add default tube option in sample conf
    * add tube in notify filter and fix logger configuration
    * missing use tube command for consumers

--- oio/event/filters/base.py
+++ oio/event/filters/base.py
@@ -1,7 +1,11 @@
+from oio.common.utils import get_logger
+
+
 class Filter(object):
-    def __init__(self, app, conf):
+    def __init__(self, app, conf, logger=None):
         self.app = app
         self.conf = conf
+        self.logger = logger or get_logger(conf)
         self.init()
 
     def init(self):
