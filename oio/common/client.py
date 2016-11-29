febc52cc5327819960b70585941d378a4134044e
Florent Vennetier (OpenIO) <florent@fridu.net>
2016-11-29 16:36:07 +0100

    Python API: allow setting connection pool options

--- oio/common/client.py
+++ oio/common/client.py
@@ -1,8 +1,10 @@
 from oio.common.exceptions import from_response
 from oio.common.http import requests
+from oio.common.http import CONNECTION_TIMEOUT, READ_TIMEOUT
 from oio.common.utils import get_logger
 from oio.common.utils import load_namespace_conf
 from oio.common.utils import validate_service_conf
+from oio.common.utils import int_value
 
 
 class Client(object):
@@ -14,14 +16,27 @@ class Client(object):
         self.conf = conf
         self.ns_conf = ns_conf
         self.logger = get_logger(conf)
-        self.session = session or requests.Session()
+        if not session:
+            session = requests.Session()
+            if "pool_maxsize" in kwargs:
+                pool_maxsize = int_value(kwargs.get("pool_maxsize"), 20)
+                adapter = requests.adapters.HTTPAdapter(
+                    pool_maxsize=pool_maxsize)
+                session.mount("http://", adapter)
+        self.session = session
         self.endpoint = 'http://%s' % ns_conf.get('proxy')
 
     def _direct_request(self, method, full_url, **kwargs):
         headers = kwargs.get('headers') or {}
         headers = dict([k, str(headers[k])] for k in headers)
         kwargs['headers'] = headers
-        resp = self.session.request(method, full_url, **kwargs)
+        if "timeout" not in kwargs:
+            resp = self.session.request(
+                method, full_url,
+                timeout=(CONNECTION_TIMEOUT, READ_TIMEOUT),
+                **kwargs)
+        else:
+            resp = self.session.request(method, full_url, **kwargs)
         try:
             body = resp.json()
         except ValueError:
