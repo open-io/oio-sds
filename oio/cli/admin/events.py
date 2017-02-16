820630908c4309dee76dc887f0112415784198b9
Florent Vennetier (OpenIO) <florent@fridu.net>
2017-02-16 16:38:53 +0100

    cli: new "events exhume" subcommand (fix #888)

--- oio/cli/admin/events.py
+++ oio/cli/admin/events.py
@@ -22,3 +22,31 @@ class StatsEvents(show.ShowOne):
         data = self.app.client_manager.admin.event_stats(
             parsed_args.tube)
         return zip(*sorted(data.iteritems()))
+
+
+class EventsExhume(show.ShowOne):
+    """
+    Exhume (replay) events that have been buried.
+    """
+
+    log = logging.getLogger(__name__ + '.EventsExhume')
+
+    def get_parser(self, prog_name):
+        parser = super(EventsExhume, self).get_parser(prog_name)
+        parser.add_argument(
+            '--tube',
+            default="oio",
+            help='Name of the tube to interact with (defaults to "oio")')
+        parser.add_argument(
+            '--limit',
+            type=int,
+            default=1000,
+            help='Maximum number of events to exhume (defaults to 1000)')
+        return parser
+
+    def take_action(self, parsed_args):
+        self.log.debug('take_action(%s)', parsed_args)
+
+        count = self.app.client_manager.admin.event.exhume(parsed_args.limit,
+                                                           parsed_args.tube)
+        return [("Exhumed",), (count,)]
