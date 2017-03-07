12f2738b8b10832946b742d2e4c2e9aeb2ce2190
Florent Vennetier (OpenIO) <florent@fridu.net>
2017-03-07 11:43:29 +0100

    cli: new options for "directory bootstrap" (fix #883)
    * --check to check the number or replicas before saving
    * --force to do the bootstrap even if already done
    * timeout is set to 30s

--- oio/cli/admin/directory.py
+++ oio/cli/admin/directory.py
@@ -14,7 +14,7 @@ class DirectoryCmd(Command):
         parser = super(DirectoryCmd, self).get_parser(prog_name)
         parser.add_argument('--replicas', metavar='<N>', dest='replicas',
                             type=int, default=3,
-                            help='Set the number of replicas')
+                            help='Set the number of replicas (3 by default)')
         return parser
 
     def get_prefix_mapping(self, parsed_args):
@@ -38,31 +38,60 @@ class DirectoryInit(DirectoryCmd):
                             default=True,
                             help='Do not assign rdir services to rawx services'
                             )
+        parser.add_argument('--force',
+                            action='store_true',
+                            help="Do the bootstrap even if already done")
+        parser.add_argument(
+            '--check',
+            action='store_true',
+            help="Check that all prefixes have the right number of replicas")
         return parser
 
     def take_action(self, parsed_args):
         self.log.debug('take_action(%s)', parsed_args)
         mapping = self.get_prefix_mapping(parsed_args)
+        mapping.load()
 
         # Bootstrap with the 'random' strategy, then rebalance with the
         # 'less_prefixes' strategy to ensure the same number of prefixes
         # per meta1. This is faster than bootstrapping directly with the
         # 'less_prefixes' strategy.
-        self.log.info("Computing meta1 prefix mapping...")
-        mapping.bootstrap()
+        if not mapping or parsed_args.force:
+            self.log.info("Computing meta1 prefix mapping...")
+            mapping.bootstrap()
+        else:
+            self.log.info("Meta1 prefix mapping already initialized")
+
         self.log.info("Equilibrating...")
         mapping.rebalance()
-        self.log.info("Saving...")
-        mapping.force()
+
+        checked = not parsed_args.check
+        if not checked:
+            self.log.info("Checking...")
+            checked = mapping.check_replicas()
+
+        if checked:
+            self.log.info("Saving...")
+            mapping.force(timeout=(5.0, 30.0))
+
         if parsed_args.rdir:
             self.log.info("Assigning rdir services to rawx services...")
             time.sleep(5)  # Let meta1 fetch the list of managed bases
             self.app.client_manager.admin.rdir_lb.assign_all_rawx()
-        self.log.info("Done")
+
+        if checked:
+            self.log.info("Done")
+        else:
+            self.log.warn("Errors encountered")
+            raise Exception("Bad meta1 prefix mapping")
 
 
 class DirectoryList(DirectoryCmd):
-    """List the content of the directory"""
+    """
+    List the content of the directory as a JSON object.
+
+    WARNING: output is >2MB.
+    """
 
     def take_action(self, parsed_args):
         self.log.debug('take_action(%s)', parsed_args)
