3a0c6513a5bc7eec6726fd388a6e1ea1052b699c
Florent Vennetier (OpenIO) <florent@fridu.net>
2017-03-21 18:06:48 +0100

    tests: new test of account stats after container deletion
    The current implementation of this test is slow, because there is
    a 5s delay before event generation.

--- oio/__init__.py
+++ oio/__init__.py
@@ -8,3 +8,7 @@ except pkg_resources.DistributionNotFound:
     _version_info = pbr.version.VersionInfo('oio')
     __version__ = _version_info.release_string()
     __canonical_version = _version_info.version_string()
+
+from oio.api.object_storage import ObjectStorageAPI as ObjectStorageApi
+
+__all__ = ["ObjectStorageApi"]
