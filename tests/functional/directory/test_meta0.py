3a2bbfb2ffc3d3d50fef3b6f0f4444e12fc438c5
Florent Vennetier (OpenIO) <florent@fridu.net>
2017-04-24 17:17:20 +0200

    tests: more directory bootstrap tests

--- tests/functional/directory/test_meta0.py
+++ tests/functional/directory/test_meta0.py
@@ -1,3 +1,4 @@
+import logging
 from tests.utils import BaseTestCase
 from oio.directory.meta0 import PrefixMapping
 from oio.directory.meta0 import Meta0Client
@@ -26,30 +27,26 @@ class TestPrefixMapping(BaseTestCase):
         self.cs_client = FakeConscienceClient()
         self.m0_client = Meta0Client({'namespace': self.ns})
 
+    def make_mapping(self, replicas=3, digits=None):
+        mapping = PrefixMapping(self.m0_client, self.cs_client,
+                                replicas=replicas, digits=digits,
+                                logger=logging.getLogger('test'))
+        return mapping
+
     def test_bootstrap_3_services(self):
         self.cs_client.generate_services(3)
-        mapping = PrefixMapping(self.m0_client, self.cs_client)
+        mapping = self.make_mapping()
         mapping.bootstrap()
         n_pfx_by_svc = mapping.count_pfx_by_svc()
         for count in n_pfx_by_svc.itervalues():
             self.assertIn(count, range(PrefixMapping.TOTAL_PREFIXES - 5000,
                                        PrefixMapping.TOTAL_PREFIXES + 5000))
 
-    def test_bootstrap_3_services_rebalanced(self):
-        self.cs_client.generate_services(3)
-        mapping = PrefixMapping(self.m0_client, self.cs_client)
-        mapping.bootstrap()
-        mapping.rebalance()
-        n_pfx_by_svc = mapping.count_pfx_by_svc()
-        for count in n_pfx_by_svc.itervalues():
-            self.assertEqual(PrefixMapping.TOTAL_PREFIXES, count)
-
     def test_bootstrap_20_services(self):
         n = 20
         replicas = 3
         self.cs_client.generate_services(n, locations=10)
-        mapping = PrefixMapping(self.m0_client, self.cs_client,
-                                replicas=replicas)
+        mapping = self.make_mapping(replicas=replicas)
         mapping.bootstrap()
         n_pfx_by_svc = mapping.count_pfx_by_svc()
         ideal = PrefixMapping.TOTAL_PREFIXES * replicas / n
@@ -58,28 +55,48 @@ class TestPrefixMapping(BaseTestCase):
         for count in n_pfx_by_svc.itervalues():
             self.assertIn(count, arange)
 
-    def test_bootstrap_20_services_rebalanced(self):
-        n = 20
-        replicas = 3
-        self.cs_client.generate_services(n, locations=7)
-        mapping = PrefixMapping(self.m0_client, self.cs_client,
-                                replicas=replicas)
+    def _test_bootstrap_rebalanced(self, n_svc, replicas,
+                                   locations=-1, digits=None):
+        if locations < 0:
+            locations = n_svc
+        self.cs_client.generate_services(n_svc, locations=locations)
+        mapping = self.make_mapping(replicas=replicas)
         mapping.bootstrap()
         mapping.rebalance()
         self.assertTrue(mapping.check_replicas())
         n_pfx_by_svc = mapping.count_pfx_by_svc()
-        ideal = PrefixMapping.TOTAL_PREFIXES * replicas / n
+        ideal = PrefixMapping.TOTAL_PREFIXES * replicas / n_svc
         arange = range(int(ideal * 0.95), int(ideal * 1.05))
         print "Ideal: ", ideal
         for count in n_pfx_by_svc.itervalues():
             self.assertIn(count, arange)
 
+    def test_bootstrap_3_services_rebalanced(self):
+        return self._test_bootstrap_rebalanced(3, 3)
+
+    def test_bootstrap_4_services_rebalanced(self):
+        return self._test_bootstrap_rebalanced(4, 3)
+
+    def test_bootstrap_6_services_rebalanced(self):
+        return self._test_bootstrap_rebalanced(6, 3)
+
+    def test_bootstrap_7_services_rebalanced(self):
+        return self._test_bootstrap_rebalanced(7, 3)
+
+    def test_bootstrap_20_services_rebalanced(self):
+        return self._test_bootstrap_rebalanced(20, 3, 7)
+
+    def test_bootstrap_3_services_3_digits_rebalanced(self):
+        return self._test_bootstrap_rebalanced(3, 3, digits=3)
+
+    def test_bootstrap_3_services_2_digits_rebalanced(self):
+        return self._test_bootstrap_rebalanced(3, 3, digits=2)
+
     def test_decommission(self):
         n = 20
         replicas = 3
         self.cs_client.generate_services(n, locations=7)
-        mapping = PrefixMapping(self.m0_client, self.cs_client,
-                                replicas=replicas)
+        mapping = self.make_mapping(replicas=replicas)
         mapping.bootstrap()
         mapping.rebalance()
         self.assertTrue(mapping.check_replicas())
