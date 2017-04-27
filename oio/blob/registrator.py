8fba3dc40480890186c2f537ce7e2465798375b1
Jean-Francois Smigielski <jf.smigielski@gmail.com>
2017-04-27 14:21:17 +0200

    oio-blob-registrator: Step 2
    - Fix some loggings
    - Add TODO comments
    - Add an initial sanity check on the NS
    - Remove odd/unused options

--- oio/blob/registrator.py
+++ oio/blob/registrator.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 
 from oio.common.utils import paths_gen
 from oio.blob.utils import check_volume, read_chunk_metadata
@@ -5,6 +6,7 @@ from oio.container.client import ContainerClient
 from oio.common import exceptions as exc
 
 
+@contextmanager
 def lock_volume(path):
     # TODO xattr-lock the volume
     yield
@@ -25,32 +27,44 @@ class BlobRegistratorWorker(object):
         self.conf = conf
         self.logger = logger
         self.volume = volume
-        self.namespace, self.volume_id = check_volume(self.volume)
-        conf = dict()
-        conf['namespace'] = self.namespace
-        self.client = ContainerClient(conf)
+        self.namespace = self.conf["namespace"]
+        self.volume_ns, self.volume_id = check_volume(self.volume)
+        c = dict()
+        c['namespace'] = self.namespace
+        self.client = ContainerClient(c)
 
     def pass_with_lock(self):
         with lock_volume(self.volume):
             return self.pass_without_lock()
 
     def pass_without_lock(self):
+        if self.namespace != self.volume_ns:
+            self.logger.warn("Forcing the NS to [%s] (previously [%s])",
+                             self.namespace, self.volume_ns)
+        # TODO(jfs): do the startup reporting
         paths = paths_gen(self.volume)
         for path in paths:
             try:
                 self._register_chunk(path)
+                # TODO(jfs): do the throttling
             except Exception as e:
                 self.logger.warn("Faulty chunk found at %s: %s", path, str(e))
+            # TODO(jfs): do the periodical reporting
+        # TODO(jfs): do the final reporting
 
     def _register_chunk(self, path):
         with open(path) as f:
             try:
                 meta = read_chunk_metadata(f)
-                self.logger.warn("%s > %s", path, meta)
+                cid = meta['container_id']
+                if str(meta['chunk_pos']).startswith('0'):
+                    if not self.conf['first']:
+                        self.logger.info("skip %s from %s", path, cid)
+                        return
                 pre = meta2bean(self.volume_id, meta)
                 post = meta2bean(self.volume_id, meta)
-                self.client.raw_update(pre, post,
-                                       cid=meta['container_id'],
+                self.client.raw_update(pre, post, cid=cid,
                                        path=meta['content_path'])
+                self.logger.info("registered %s in %s", path, cid)
             except exc.MissingAttribute as e:
                 raise exc.FaultyChunk('Missing extended attribute %s' % e)
