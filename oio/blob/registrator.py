0f0d05b206cbc30ff59b5e6fa1d078acc76c084d
Jean-Francois Smigielski <jf.smigielski@gmail.com>
2017-05-03 20:34:46 +0200

    oio-blob-registrator: Add an insert/update/check trigger

--- oio/blob/registrator.py
+++ oio/blob/registrator.py
@@ -1,9 +1,14 @@
 from contextlib import contextmanager
+from os.path import basename
+from time import clock as now
 
 from oio.common.utils import paths_gen
 from oio.blob.utils import check_volume, read_chunk_metadata
 from oio.container.client import ContainerClient
-from oio.common import exceptions as exc
+from oio.common.exceptions import Conflict, NotFound
+
+
+default_report_interval = 60.0
 
 
 @contextmanager
@@ -32,39 +37,92 @@ class BlobRegistratorWorker(object):
         c = dict()
         c['namespace'] = self.namespace
         self.client = ContainerClient(c)
+        self.report_interval = conf.get(
+                "report_period", default_report_interval)
+
+        actions = {
+                'update': BlobRegistratorWorker._update_chunk,
+                'insert': BlobRegistratorWorker._insert_chunk,
+                'check': BlobRegistratorWorker._check_chunk,
+        }
+        self.action = actions[conf.get("action", "check")]
 
     def pass_with_lock(self):
         with lock_volume(self.volume):
             return self.pass_without_lock()
 
     def pass_without_lock(self):
+        last_report = now()
+        count, success, fail = 0, 0, 0
         if self.namespace != self.volume_ns:
             self.logger.warn("Forcing the NS to [%s] (previously [%s])",
                              self.namespace, self.volume_ns)
-        # TODO(jfs): do the startup reporting
+
+        self.logger.info("START %s", self.volume)
+
         paths = paths_gen(self.volume)
         for path in paths:
+            # Action
             try:
-                self._register_chunk(path)
-                # TODO(jfs): do the throttling
+                with open(path) as f:
+                    meta = read_chunk_metadata(f)
+                    self.action(self, path, f, meta)
+                    success = success + 1
+            except NotFound as e:
+                fail = fail + 1
+                self.logger.info("ORPHAN %s/%s in %s/%s %s",
+                                 meta['content_id'], meta['chunk_id'],
+                                 meta['container_id'], meta['content_path'],
+                                 str(e))
+            except Conflict as e:
+                fail = fail + 1
+                self.logger.info("ALREADY %s/%s in %s/%s %s",
+                                 meta['content_id'], meta['chunk_id'],
+                                 meta['container_id'], meta['content_path'],
+                                 str(e))
             except Exception as e:
-                self.logger.warn("Faulty chunk found at %s: %s", path, str(e))
-            # TODO(jfs): do the periodical reporting
-        # TODO(jfs): do the final reporting
+                fail = fail + 1
+                self.logger.warn("ERROR %s/%s in %s/%s %s",
+                                 meta['content_id'], meta['chunk_id'],
+                                 meta['container_id'], meta['content_path'],
+                                 str(e))
+            count = count + 1
 
-    def _register_chunk(self, path):
-        with open(path) as f:
-            try:
-                meta = read_chunk_metadata(f)
-                cid = meta['container_id']
-                if str(meta['chunk_pos']).startswith('0'):
-                    if not self.conf['first']:
-                        self.logger.info("skip %s from %s", path, cid)
-                        return
-                pre = meta2bean(self.volume_id, meta)
-                post = meta2bean(self.volume_id, meta)
-                self.client.raw_update(pre, post, cid=cid,
-                                       path=meta['content_path'])
-                self.logger.info("registered %s in %s", path, cid)
-            except exc.MissingAttribute as e:
-                raise exc.FaultyChunk('Missing extended attribute %s' % e)
+            # TODO(jfs): do the throttling
+
+            # periodical reporting
+            t = now()
+            if t - last_report > self.report_interval:
+                self.logger.info("STEP %d ok %d ko %d",
+                                 count, success, fail)
+
+        self.logger.info("FINAL %s %d ok %d ko %d",
+                         self.volume, count, success, fail)
+
+    def _check_chunk(self, path, f, meta):
+        raise Exception("CHECK not yet implemented")
+
+    def _insert_chunk(self, path, f, meta):
+        cid = meta['container_id']
+        chunkid = basename(path)
+        bean = meta2bean(self.volume_id, meta)
+        self.client.container_raw_insert(bean, cid=cid)
+        self.logger.info("inserted %s/%s in %s/%s",
+                         meta['content_id'], chunkid, cid,
+                         meta['content_path'])
+
+    def _update_chunk(self, path, f, meta):
+        cid = meta['container_id']
+        chunkid = basename(path)
+        if str(meta['chunk_pos']).startswith('0'):
+            if not self.conf['first']:
+                self.logger.info("skip %s/%s from %s/%s",
+                                 meta['content_id'], chunkid, cid,
+                                 meta['content_path'])
+                return
+        pre = meta2bean(self.volume_id, meta)
+        post = meta2bean(self.volume_id, meta)
+        self.client.container_raw_update(pre, post, cid=cid)
+        self.logger.info("updated %s/%s in %s/%s",
+                         meta['content_id'], chunkid, cid,
+                         meta['content_path'])
