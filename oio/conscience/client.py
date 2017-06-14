56de168db59dc62f877c67f7b5eef19a7a7655eb
Florent Vennetier (OpenIO) <florent@fridu.net>
2017-06-14 17:52:56 +0200

    Python API: improve rdir assignation
    Use a special service pool, declared when needed by the CLI.
    No need for user_is_a_service anymore.

--- oio/conscience/client.py
+++ oio/conscience/client.py
@@ -51,6 +51,23 @@ class LbClient(ProxyClient):
         else:
             raise OioException("Failed to poll %s: %s" % (pool, resp.text))
 
+    def create_pool(self, pool, targets, options=None):
+        """
+        Create a service pool on the local proxy.
+
+        :param pool: a name for the pool
+        :type pool: `str`
+        :param targets: a list of tuples like (1, "rawx-usa", "rawx", ...)
+        :param options: options for the pool
+        :type options: `dict`
+        :exception Conflict: if a pool with same name already exists
+        """
+        stargets = ";".join(','.join(str(y) for y in x) for x in targets)
+        ibody = {'targets': stargets, 'options': options}
+        _, _ = self._request('POST', "/create_pool",
+                             params={'name': pool},
+                             data=json.dumps(ibody))
+
 
 class ConscienceClient(ProxyClient):
     """Conscience client. Some calls are actually redirected to LbClient."""
