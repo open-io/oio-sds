9554af5e1b153ae23892b9f48ac7d0278ee0241e
Florent Vennetier (OpenIO) <florent@fridu.net>
2017-06-15 12:22:02 +0200

    CLI: set formatter to 'value' only when doing long listings
    Formatter can be set to something else by specifying '-f something'
    AFTER '--no-paging'.

--- oio/cli/utils.py
+++ oio/cli/utils.py
@@ -10,3 +10,14 @@ class KeyValueAction(argparse.Action):
             getattr(namespace, self.dest, {}).update([values.split('=', 1)])
         else:
             getattr(namespace, self.dest, {}).pop(values, None)
+
+
+class ValueFormatStoreTrueAction(argparse.Action):
+    """Same as 'store_true', but also set 'formatter' field to 'value'"""
+    def __init__(self, option_strings, dest, nargs=0, **kwargs):
+        super(ValueFormatStoreTrueAction, self).__init__(
+              option_strings, dest, nargs=nargs, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, True)
+        setattr(namespace, "formatter", "value")
