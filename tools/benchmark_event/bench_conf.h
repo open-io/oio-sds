96ee6a226034a27ef72c1232510ed9736f20f862
Aymeric Ducroquetz <aymeric.ducroquetz@etudiant.univ-lille1.fr>
2017-06-28 10:38:09 +0200

    oio-event-benchmark: Create event_benchmark with send_events and fake_service

---
+++ tools/benchmark_event/bench_conf.h
@@ -0,0 +1,8 @@
+#ifndef OIO_SDS__tools__benchmark_event__bench_conf_h
+#define OIO_SDS__tools__benchmark_event__bench_conf_h
+
+#define NAME_SPACE "OPENIO"
+#define RAWX_ADDRESS "127.0.0.1:4444"
+#define RDIR_ADDRESS "127.0.0.1:4445"
+
+#endif /* OIO_SDS__tools__benchmark_event__bench_conf_h */
---
+++ tools/benchmark_event/bench_event_agent.c
@@ -0,0 +1,115 @@
+#include <glib.h>
+
+#include <metautils/lib/metautils.h>
+
+#include "fake_service.h"
+#include "send_events.h"
+
+static void grid_main_specific_stop (void);
+
+// send_events.c
+extern gint n_events_per_exp;
+extern gint n_experiences;
+
+GThread *fake_service_thread = NULL;
+
+static gpointer
+_fake_service_run(gpointer p)
+{
+	(void) p;
+	
+	if (!fake_service_run()) {
+		grid_main_specific_stop();
+		
+		return NULL;
+	}
+	
+	return NULL;
+}
+
+// Main callbacks
+
+static struct grid_main_option_s *
+grid_main_get_options (void)
+{
+    static struct grid_main_option_s cli_options[] = {
+		{
+			"NExp", OT_UINT, {.i = &n_experiences},
+			"Number of experiences"
+		},
+		{
+			"NEventsPerExp", OT_UINT, {.i = &n_events_per_exp},
+			"Number of events per n_experience"
+		},
+		{NULL, 0, {.i=0}, NULL}
+	};
+
+	return cli_options;
+}
+
+static const char *
+grid_main_get_usage (void)
+{
+	return "CHUNK_NEW/CHUNK_DELETE";
+}
+
+static void
+grid_main_set_defaults (void)
+{
+    send_events_defaults();
+}
+
+static gboolean
+grid_main_configure (int argc, char **argv)
+{
+    return fake_service_configure() && send_events_configure(argc, argv);
+}
+
+static void
+grid_main_action (void)
+{
+	GError *err;
+	fake_service_thread = g_thread_try_new("fake_service", _fake_service_run, NULL, 
+													&err);
+	if (fake_service_thread == NULL) {
+		GRID_INFO("Failed to start the fake_service thread: (%d) %s", err->code,
+		          err->message);
+	}
+	
+	g_usleep(G_TIME_SPAN_SECOND);
+	
+	send_events_run();
+	
+	fake_service_stop();
+	
+	g_thread_join(fake_service_thread);
+}
+
+static void
+grid_main_specific_stop (void)
+{
+	fake_service_stop();
+}
+
+static void
+grid_main_specific_fini (void)
+{
+	send_events_fini();
+	fake_service_fini();
+}
+
+struct grid_main_callbacks main_callbacks = {
+	.options = grid_main_get_options,
+	.action = grid_main_action,
+	.set_defaults = grid_main_set_defaults,
+	.specific_fini = grid_main_specific_fini,
+	.configure = grid_main_configure,
+	.usage = grid_main_get_usage,
+	.specific_stop = grid_main_specific_stop,
+};
+
+int
+main (int argc, char **argv)
+{
+    return grid_main (argc, argv, &main_callbacks);
+}
