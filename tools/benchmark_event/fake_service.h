6b28f3d0b09a489207baaf27a8499729c547446b
Aymeric Ducroquetz <aymeric.ducroquetz@etudiant.univ-lille1.fr>
2017-06-28 10:29:33 +0200

    oio-event-benchmark: Create fake service to catch the events

---
+++ tools/benchmark_event/fake_service.h
@@ -0,0 +1,14 @@
+#ifndef OIO_SDS__tools__benchmark_event__fake_service_h
+#define OIO_SDS__tools__benchmark_event__fake_service_h
+
+#include <glib.h>
+
+gboolean fake_service_configure (void);
+
+gboolean fake_service_run (void);
+
+void fake_service_stop (void);
+
+void fake_service_fini (void);
+
+#endif /* OIO_SDS__tools__benchmark_event__fake_service_h */
