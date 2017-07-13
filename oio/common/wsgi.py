9b39ff8cd4b495d5b3c76f3f5c81e6879172137b
Florent Vennetier (OpenIO) <florent@fridu.net>
2017-07-13 16:34:53 +0200

    account,container,ecd: improve exception handling

--- oio/common/wsgi.py
+++ oio/common/wsgi.py
@@ -1,5 +1,20 @@
+# Copyright (C) 2015-2017 OpenIO SAS
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3.0 of the License, or (at your option) any later version.
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.
+
 from gunicorn.app.base import BaseApplication
 from gunicorn.glogging import Logger
+from werkzeug.wrappers import Request
+from werkzeug.exceptions import HTTPException, InternalServerError
 
 from oio.common.utils import get_logger, read_conf, CPU_COUNT
 
@@ -74,6 +89,33 @@ class ServiceLogger(Logger):
         # do not log status requests
         if environ.get('PATH_INFO', '/') != '/status':
             super(ServiceLogger, self).access(resp, req, environ, request_time)
+
+
+class WerkzeugApp(object):
+
+    def __init__(self, url_map=None, logger=None):
+        self.url_map = url_map
+        self.logger = logger or get_logger(None)
+
+    def dispatch_request(self, req):
+        adapter = self.url_map.bind_to_environ(req.environ)
+        try:
+            endpoint, _ = adapter.match()
+            return getattr(self, 'on_' + endpoint)(req)
+        except HTTPException as exc:
+            return exc
+        except Exception as exc:
+            if self.logger:
+                self.logger.exception('ERROR Unhandled exception in request')
+            return InternalServerError('Unmanaged error: %s' % exc)
+
+    def wsgi_app(self, environ, start_response):
+        req = Request(environ)
+        resp = self.dispatch_request(req)
+        return resp(environ, start_response)
+
+    def __call__(self, environ, start_response):
+        return self.wsgi_app(environ, start_response)
 
 
 def init_request_processor(conf_file, app_name, app_factory, *args, **kwargs):
