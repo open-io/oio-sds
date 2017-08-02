570a7adc762202f3e2d32fb19adb3eeb4dc8fc54
Jean-Francois Smigielski <jf.smigielski@gmail.com>
2017-08-02 12:23:13 +0200

    Re-licenses to Affero cache/ and some test-reltaed files

--- tests/unit/test_lb.c
+++ tests/unit/test_lb.c
@@ -2,18 +2,18 @@
 OpenIO SDS unit tests
 Copyright (C) 2016-2017 OpenIO, as part of OpenIO SDS
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 3.0 of the License, or (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
 
-This library is distributed in the hope that it will be useful,
+This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public
-License along with this library.
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <stdlib.h>
