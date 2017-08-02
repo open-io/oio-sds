a8c1b9da3d31712605b87732354fdafd4daec5d8
Jean-Francois Smigielski <jf.smigielski@gmail.com>
2017-08-02 17:12:23 +0200

    Add the Licence preamble (LGPL) in the tests
    ... where it was missing, and turns several tests to LGPL, that were
    previously with AGPL (OpenIO-owned at 100%).

--- tests/functional/crawler/test_storage_tierer.py
+++ tests/functional/crawler/test_storage_tierer.py
@@ -1,5 +1,4 @@
-# Copyright (C) 2015-2017 OpenIO SAS, as part of
-# OpenIO Software Defined Storage
+# Copyright (C) 2015-2017 OpenIO SAS, as part of OpenIO SDS
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
