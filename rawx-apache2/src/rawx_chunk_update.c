efebd881066cbd14d110a4bdd6d7c74469012de7
Jean-Francois Smigielski <jf.smigielski@gmail.com>
2017-11-21 22:47:15 +0100

    Document the RAWX network interface

--- rawx-apache2/src/rawx_chunk_update.c
+++ rawx-apache2/src/rawx_chunk_update.c
@@ -95,6 +95,96 @@ __get_chunkupdate_resource(const request_rec *r, const dav_hooks_repository *hoo
 	return resource;
 }
 
+/*
+RAWX{{
+GET /update/{CHUNKID}
+~~~~~~~~~~~~~~~~~~~~~
+
+Despite the GET verb, this route alters the metadata of the chunk.
+
+.. list-table:: URL tokens
+   :header-rows: 1
+   :widths: 10 20
+
+   * - Token
+     - Description
+   * - CHUNKID
+     - a string of 64 hexadecimal characters
+
+
+.. list-table:: Headers
+   :header-rows: 1
+   :widths: 10 20
+
+   * - Header
+     - Description
+   * - X-oio-chunk-meta-container-id
+     - String of 64 hexadecimal characters
+   * - X-oio-chunk-meta-content-path
+     - A string
+   * - X-oio-chunk-meta-content-id
+     - String of hexadecimal characters, usually of 32 characters. The only constraint
+       is that the number must be even, to be convertible to a binary form.
+   * - X-oio-chunk-meta-content-version
+     - A strictly positive integer, less than (2^63). That integer SHOULD match the
+       number of bytes of the targeted chunk.
+   * - X-oio-chunk-meta-content-storage-policy
+     - A string, no check will be performed, at the RAWX level the string might even
+       represent a non-existing storage policy
+   * - X-oio-chunk-meta-content-chunk-method
+     - A string. At the RAWX level no check will be performed and that string
+       might even be an invalid chunk-method description.
+   * - X-oio-chunk-meta-metachunk-size
+     - A null or positive number
+   * - X-oio-chunk-meta-metachunk-hash
+     - A string of 32 hexadecimal characters
+   * - X-oio-chunk-meta-chunk-id
+     - A string of 64 hexadecimal characters that must math the CHUNKID present in
+       the URL
+   * - X-oio-chunk-meta-chunk-size
+     - A null or positive integer
+   * - X-oio-chunk-meta-chunk-hash
+     - A string of 32 hexadecimal characters
+   * - X-oio-chunk-meta-chunk-pos
+     - Either a positive integer (including 0) or a compound of 2 positive integers
+       gathered with a dot.
+   * - X-oio-chunk-meta-oio-version
+     - A string describing the versin of the headers.
+   * - X-oio-chunk-meta-full-path
+     - A string representing a complete OpenIO URL
+
+
+Example
+-------
+
+.. code-block:: http
+
+   GET /update/0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF HTTP/1.1
+   Content-Length: 0
+   X-oio-chunk-meta-container-id: 9006CE70B59E5777D6BB410C57944812EB05FCDB5BA85D520A14B3051D1D094F
+   X-oio-chunk-meta-content-path: magic
+   X-oio-chunk-meta-content-id: 5835AF8D805E0500AAB7F6808F50900A
+   X-oio-chunk-meta-content-version: 1511281109448048
+   X-oio-chunk-meta-content-storage-policy: EC
+   X-oio-chunk-meta-content-chunk-method: ec/algo=liberasurecode_rs_vand,k=6,m=3
+   X-oio-chunk-meta-metachunk-size: 111
+   X-oio-chunk-meta-metachunk-hash: 272913026300E7AE9B5E2D51F138E674
+   X-oio-chunk-meta-chunk-id: CE456217C7DBAC618A7F0EBFBCDB6C8F184ED8ADCBC6F0B6F493A51EE095D86A
+   X-oio-chunk-meta-chunk-size: 100
+   X-oio-chunk-meta-chunk-hash: 527EC56D67EF8DA68E3FB93158552272
+   X-oio-chunk-meta-chunk-pos: 0.3
+   X-oio-chunk-meta-oio-version: 4.0
+   X-oio-chunk-meta-full-path: ACCT/JFS/magic/1511281109448048
+
+
+
+.. code-block:: http
+
+   HTTP/1.1 200 OK
+   Content-Length: 0
+
+}}RAWX
+ */
 dav_error *
 dav_rawx_chunk_update_get_resource(request_rec *r, const char *root_dir, const char *label,
 	int use_checked_in, dav_resource **result_resource)
--- rawx-apache2/src/rawx_repository.c
+++ rawx-apache2/src/rawx_repository.c
@@ -141,6 +141,246 @@ static const dav_liveprop_group dav_rawx_liveprop_group =
  ** REPOSITORY HOOK FUNCTIONS
  */
 
+/*
+RAWX{{
+GET /{CHUNKID}
+~~~~~~~~~~~~~~
+
+Download a chunk from the target RAWX service.
+The chunk ID is given as a token of the URL.
+No particular header is expected in the request.
+The attributes of the chunk will be returned as headers of the reply.
+
+.. list-table:: URL tokens
+   :header-rows: 1
+   :widths: 10 20
+
+   * - Token
+     - Description
+   * - CHUNKID
+     - a string of 64 hexadecimal characters
+
+
+.. list-table:: Request Headers
+   :header-rows: 1
+   :widths: 10 20
+
+   * - Header
+     - Description
+   * - Range
+     - Both *chunked* and *inline* are managed
+
+
+.. list-table:: Reply Headers
+   :header-rows: 1
+   :widths: 10 20
+
+   * - Header
+     - Description
+   * - X-oio-chunk-meta-container-id
+     - String of 64 hexadecimal characters
+   * - X-oio-chunk-meta-content-path
+     - A string
+   * - X-oio-chunk-meta-content-id
+     - String of hexadecimal characters, usually of 32 characters. The only constraint
+       is that the number must be even, to be convertible to a binary form.
+   * - X-oio-chunk-meta-content-version
+     - A strictly positive integer, less than (2^63). That integer SHOULD match the
+       number of bytes of the targeted chunk.
+   * - X-oio-chunk-meta-content-storage-policy
+     - A string, no check will be performed, at the RAWX level the string might even
+       represent a non-existing storage policy
+   * - X-oio-chunk-meta-content-chunk-method
+     - A string. At the RAWX level no check will be performed and that string
+       might even be an invalid chunk-method description.
+   * - X-oio-chunk-meta-metachunk-size
+     - A null or positive number
+   * - X-oio-chunk-meta-metachunk-hash
+     - A string of 32 hexadecimal characters
+   * - X-oio-chunk-meta-chunk-id
+     - A string of 64 hexadecimal characters that must math the CHUNKID present in
+       the URL
+   * - X-oio-chunk-meta-chunk-size
+     - A null or positive integer
+   * - X-oio-chunk-meta-chunk-hash
+     - A string of 32 hexadecimal characters
+   * - X-oio-chunk-meta-chunk-pos
+     - Either a positive integer (including 0) or a compound of 2 positive integers
+       gathered with a dot.
+   * - X-oio-chunk-meta-oio-version
+     - A string describing the versin of the headers.
+   * - X-oio-chunk-meta-full-path
+     - A string representing a complete OpenIO URL
+
+
+Example
+-------
+
+.. code-block:: http
+
+   GET /CE456217C7DBAC618A7F0EBFBCDB6C8F184ED8ADCBC6F0B6F493A51EE095D86A HTTP/1.1
+   Host: 127.0.0.1:6009
+   User-Agent: curl/7.55.1
+
+
+.. code-block:: http
+
+   HTTP/1.1 200 OK
+   Date: Tue, 21 Nov 2017 16:19:57 GMT
+   Server: Apache
+   Last-Modified: Tue, 21 Nov 2017 16:18:29 GMT
+   ETag: "55e808daf7710"
+   Accept-Ranges: bytes
+   Content-Length: 100
+   X-oio-chunk-meta-container-id: 9006CE70B59E5777D6BB410C57944812EB05FCDB5BA85D520A14B3051D1D094F
+   X-oio-chunk-meta-content-path: magic
+   X-oio-chunk-meta-content-id: 5835AF8D805E0500AAB7F6808F50900A
+   X-oio-chunk-meta-content-version: 1511281109448048
+   X-oio-chunk-meta-content-storage-policy: EC
+   X-oio-chunk-meta-content-chunk-method: ec/algo=liberasurecode_rs_vand,k=6,m=3
+   X-oio-chunk-meta-metachunk-size: 111
+   X-oio-chunk-meta-metachunk-hash: 272913026300E7AE9B5E2D51F138E674
+   X-oio-chunk-meta-chunk-id: CE456217C7DBAC618A7F0EBFBCDB6C8F184ED8ADCBC6F0B6F493A51EE095D86A
+   X-oio-chunk-meta-chunk-size: 100
+   X-oio-chunk-meta-chunk-hash: 527EC56D67EF8DA68E3FB93158552272
+   X-oio-chunk-meta-chunk-pos: 0.3
+   X-oio-chunk-meta-oio-version: 4.0
+   X-oio-chunk-meta-full-path: ACCT/JFS/magic/1511281109448048
+
+   ...
+
+
+PUT /{CHUNKID}
+~~~~~~~~~~~~~~
+
+Upload a chunk on the target RAWX service.
+
+.. list-table:: URL tokens
+   :header-rows: 1
+   :widths: 10 20
+
+   * - Token
+     - Description
+   * - CHUNKID
+     - a string of 64 hexadecimal characters
+
+
+.. list-table:: HTTP Headers
+   :header-rows: 1
+   :widths: 10 20
+
+   * - Header
+     - Description
+   * - Transfer-Encoding
+     - Both *chunked* and *inline* are managed
+
+
+.. list-table:: RAWX Headers
+   :header-rows: 1
+   :widths: 10 20
+
+   * - Header
+     - Description
+   * - X-oio-chunk-meta-container-id
+     - String of 64 hexadecimal characters
+   * - X-oio-chunk-meta-content-path
+     - A string
+   * - X-oio-chunk-meta-content-id
+     - String of hexadecimal characters, usually of 32 characters. The only constraint
+       is that the number must be even, to be convertible to a binary form.
+   * - X-oio-chunk-meta-content-version
+     - A strictly positive integer, less than (2^63). That integer SHOULD match the
+       number of bytes of the targeted chunk.
+   * - X-oio-chunk-meta-content-storage-policy
+     - A string, no check will be performed, at the RAWX level the string might even
+       represent a non-existing storage policy
+   * - X-oio-chunk-meta-content-chunk-method
+     - A string. At the RAWX level no check will be performed and that string
+       might even be an invalid chunk-method description.
+   * - X-oio-chunk-meta-metachunk-size
+     - A null or positive number
+   * - X-oio-chunk-meta-metachunk-hash
+     - A string of 32 hexadecimal characters
+   * - X-oio-chunk-meta-chunk-id
+     - A string of 64 hexadecimal characters that must math the CHUNKID present in
+       the URL
+   * - X-oio-chunk-meta-chunk-size
+     - A null or positive integer
+   * - X-oio-chunk-meta-chunk-hash
+     - A string of 32 hexadecimal characters
+   * - X-oio-chunk-meta-chunk-pos
+     - Either a positive integer (including 0) or a compound of 2 positive integers
+       gathered with a dot.
+   * - X-oio-chunk-meta-oio-version
+     - A string describing the versin of the headers.
+   * - X-oio-chunk-meta-full-path
+     - A string representing a complete OpenIO URL
+
+
+Example
+-------
+
+.. code-block:: http
+
+   PUT /0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF HTTP/1.1
+   Content-Length: 12345
+   X-oio-chunk-meta-container-id: 9006CE70B59E5777D6BB410C57944812EB05FCDB5BA85D520A14B3051D1D094F
+   X-oio-chunk-meta-content-path: magic
+   X-oio-chunk-meta-content-id: 5835AF8D805E0500AAB7F6808F50900A
+   X-oio-chunk-meta-content-version: 1511281109448048
+   X-oio-chunk-meta-content-storage-policy: EC
+   X-oio-chunk-meta-content-chunk-method: ec/algo=liberasurecode_rs_vand,k=6,m=3
+   X-oio-chunk-meta-metachunk-size: 111
+   X-oio-chunk-meta-metachunk-hash: 272913026300E7AE9B5E2D51F138E674
+   X-oio-chunk-meta-chunk-id: CE456217C7DBAC618A7F0EBFBCDB6C8F184ED8ADCBC6F0B6F493A51EE095D86A
+   X-oio-chunk-meta-chunk-size: 100
+   X-oio-chunk-meta-chunk-hash: 527EC56D67EF8DA68E3FB93158552272
+   X-oio-chunk-meta-chunk-pos: 0.3
+   X-oio-chunk-meta-oio-version: 4.0
+   X-oio-chunk-meta-full-path: ACCT/JFS/magic/1511281109448048
+
+   -snip-
+
+
+.. code-block:: http
+
+   HTTP/1.1 200 OK
+   Content-Length: 0
+
+
+
+DELETE /{CHUNKID}
+~~~~~~~~~~~~~~~~~
+
+Delete a chunk stored on the target RAWX service.
+
+.. list-table:: URL tokens
+   :header-rows: 1
+   :widths: 10 20
+
+   * - Token
+     - Description
+   * - CHUNKID
+     - a string of 64 hexadecimal characters
+
+
+Example
+-------
+
+.. code-block:: http
+
+   DELETE /0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF HTTP/1.1
+   Content-Length: 0
+
+
+.. code-block:: http
+
+   HTTP/1.1 204 No content
+   Content-Length: 0
+
+
+}}RAWX
+ */
 static dav_error *
 dav_rawx_get_resource(request_rec *r, const char *root_dir, const char *label,
 	int use_checked_in, dav_resource **result_resource)
