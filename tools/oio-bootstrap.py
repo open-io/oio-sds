f77e35ba2e061813c214e712c12079befc75efd2
Loïc Carr <lcarr@online.net>
2017-11-22 22:10:23 +0100

    Update oio-bootstrap.py for python3

--- tools/oio-bootstrap.py
+++ tools/oio-bootstrap.py
@@ -18,6 +18,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import print_function
+from six import iterkeys, iteritems
+from six.moves import xrange
 
 import errno
 import grp
@@ -1054,7 +1056,7 @@ def gridinit(env):
 
 def mkdir_noerror(d):
     try:
-        os.makedirs(d, 0700)
+        os.makedirs(d, 0o700)
     except OSError as e:
         if e.errno != errno.EEXIST:
             raise e
@@ -1331,7 +1333,7 @@ def generate(options):
         tpl = Template(tpl)
         with open(gridinit(env), 'a+') as f:
             f.write(tpl.safe_substitute(env))
-            for key in (k for k in env.iterkeys() if k.startswith("env.")):
+            for key in (k for k in iterkeys(env) if k.startswith("env.")):
                 f.write("%s=%s\n" % (key, env[key]))
         # watcher
         tpl = Template(template_meta_watch)
@@ -1453,7 +1455,7 @@ def generate(options):
     with open(gridinit(env), 'a+') as f:
         tpl = Template(template_gridinit_proxy)
         f.write(tpl.safe_substitute(env))
-        for key in (k for k in env.iterkeys() if k.startswith("env.")):
+        for key in (k for k in iterkeys(env) if k.startswith("env.")):
             f.write("%s=%s\n" % (key, env[key]))
     with open(watch(env), 'w+') as f:
         tpl = Template(template_proxy_watch)
@@ -1592,7 +1594,7 @@ def generate(options):
         tpl = Template(template_local_ns)
         f.write(tpl.safe_substitute(env))
         # Now dump the configuration
-        for k, v in options['config'].iteritems():
+        for k, v in iteritems(options['config']):
             strv = str(v)
             if isinstance(v, bool):
                 strv = strv.lower()
@@ -1649,7 +1651,7 @@ def dump_config(conf):
 
 
 def merge_config(base, inc):
-    for k, v in inc.iteritems():
+    for k, v in iteritems(inc):
         if isinstance(v, dict):
             if k not in base:
                 base[k] = v
