9e68263598f4053f6cb91c0e1f2c6a5c2cbe177d
Florent Vennetier (OpenIO) <florent@fridu.net>
2017-12-01 10:55:00 +0100

    Do not fail pkg-config version test on master branch

--- tools/oio-check-version.sh
+++ tools/oio-check-version.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
 # This will work in the main repository, but not in forks
 LATEST_TAG=$(git fetch --tags && git describe --tags --first-parent)
 if [ -z "$LATEST_TAG" ]
@@ -8,6 +10,7 @@ then
   exit 0
 fi
 
+echo "current branch is                  $CURRENT_BRANCH"
 echo "latest tag is                      $LATEST_TAG"
 export LATEST_TAG
 SHORT_VERSION=$(echo "$LATEST_TAG" | sed -E 's/^([[:digit:]]+\.)([[:digit:]]+).*$/\1\2/')
@@ -16,14 +19,23 @@ PKG_CONFIG_PATH=/tmp/oio/lib/pkgconfig
 export PKG_CONFIG_PATH
 PKGCONFIG_VERSION=$(pkg-config --modversion oio-sds)
 
-echo "oio-sds short version is           $SHORT_VERSION"
+echo "oio-sds version from tag is        $SHORT_VERSION"
 echo "oio-sds version from pkg-config is $PKGCONFIG_VERSION"
 
 # Ensure pkg-config version is up-to-date
-if [ "$SHORT_VERSION" == "$PKGCONFIG_VERSION" ]
+if [ "$SHORT_VERSION" = "$PKGCONFIG_VERSION" ]
 then
   echo "OK"
 else
+  if [ "${CURRENT_BRANCH}" = "master" ]
+  then
+    echo "Ignoring version mismatch as we are on master branch."
+    exit 0
+  elif [ "${CURRENT_BRANCH}" = "HEAD" ]
+  then
+    echo "Ignoring version mismatch as we are on a detached head."
+    exit 0
+  fi
   echo "KO"
   exit 1
 fi
