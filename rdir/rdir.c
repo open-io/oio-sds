a29fa05acb7379bc531dc1b403b6d24cf488061d
Michael Bonfils <murlock42@gmail.com>
2018-01-09 18:02:02 +0100

    rdir: prepare UUID management for rdir
    At this time, it use config UUID/Service ID.
    It will be exposed through /status

--- rdir/rdir.c
+++ rdir/rdir.c
@@ -36,6 +36,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 static gchar *ns_name = NULL;
 static gchar *basedir = NULL;
+static gchar *uuid = NULL;
 static gboolean config_system = TRUE;
 static GSList *config_paths = NULL;
 static GSList *config_urlv = NULL;
@@ -1255,6 +1256,10 @@ _route_srv_status(struct req_args_s *args)
 	GString *gstr = g_string_sized_new(128);
 	g_string_append_c(gstr, '{');
 	oio_str_gstring_append_json_pair_int(gstr, "opened_db_count", count);
+	if (uuid) {
+		g_string_append_c(gstr, ',');
+		oio_str_gstring_append_json_pair(gstr, "uuid", uuid);
+	}
 	g_string_append_c(gstr, '}');
 
 	return _reply_ok(args->rp, gstr);
@@ -1533,6 +1538,7 @@ grid_main_specific_fini(void)
 	g_cond_clear(&cond_bases);
 	g_mutex_clear(&lock_bases);
 	oio_str_clean(&basedir);
+	oio_str_clean(&uuid);
 }
 
 static void
@@ -1596,6 +1602,8 @@ grid_main_configure(int argc, char **argv)
 		g_key_file_free(gkf);
 		return _config_error("DB path", err);
 	}
+
+	uuid = CFG("uuid");
 
 	gchar *cfg_ip = CFG("bind_addr");
 	STRING_STACKIFY(cfg_ip);
