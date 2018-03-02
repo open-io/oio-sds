open-io/oio-sds

commit b17504724c68bc49a1379ddce1ae085cacb4d2fb

sqliterepo: add timestamps in election debug logs
These timestamps are milliseconds from Unix epoch.

```
            "log": [
                "1517326850602:CHECKING_MASTER:GETVERS_OK:SLAVE",
                "1517326850597:ASKING:MASTER_OK:CHECKING_MASTER",
                "1517326850596:LISTING:LIST_OK:ASKING",
                "1517326850595:WATCHING:EXISTS_OK:LISTING",
                "1517326850589:CREATING:CREATE_OK:WATCHING",
                "1517326850556:NONE:NONE:CREATING"
            ],
```

sqliterepo/election.c
@@ -146,6 +146,7 @@ struct logged_event_s
 	enum event_type_e event   :8;
 	enum election_step_e pre  :4;
 	enum election_step_e post :4;
+	gint64 time              :48;  // cheeseparing economies
 } __attribute__ ((packed));
 
 /* @private */
@@ -963,6 +964,7 @@ member_log_event(struct election_member_s *member, enum election_step_e pre,
 	plog->event = evt;
 	plog->pre = pre;
 	plog->post = member->step;
+	plog->time = (oio_ext_real_time() / G_TIME_SPAN_MILLISECOND) % (1L << 48);
 }
 
 #ifdef HAVE_EXTRA_DEBUG
@@ -1170,11 +1172,11 @@ member_json (struct election_member_s *m, GString *gs)
 	g_string_append_c (gs, ',');
 	OIO_JSON_append_str (gs, "zk", m->key);
 	g_string_append_static (gs, "},\"#\":{");
-	OIO_JSON_append_int (gs, "R", m->refcount);
+	OIO_JSON_append_int (gs, "refcount", m->refcount);
 	g_string_append_c (gs, ',');
-	OIO_JSON_append_int (gs, "P", m->pending_PIPEFROM);
+	OIO_JSON_append_int (gs, "pipefrom", m->pending_PIPEFROM);
 	g_string_append_c (gs, ',');
-	OIO_JSON_append_int (gs, "V", m->pending_GETVERS);
+	OIO_JSON_append_int (gs, "getvers", m->pending_GETVERS);
 	g_string_append_c (gs, '}');
 
 	/* the peers */
@@ -1207,7 +1209,8 @@ member_json (struct election_member_s *m, GString *gs)
 			break;
 		if (i!=0)
 			g_string_append_c(gs, ',');
-		g_string_append_printf(gs, "\"%s:%s:%s\"", _step2str(plog->pre),
+		g_string_append_printf(gs, "\"%"G_GINT64_FORMAT":%s:%s:%s\"",
+				(gint64)plog->time, _step2str(plog->pre),
 				_evt2str(plog->event), _step2str(plog->post));
 	}
 	g_string_append_static (gs, "]}");

commit 6d372f82bede1313f14e8ec8de3f343d6a242738

Fix 32 bits build, for example armhf
[ 50%] Building C object sqliterepo/CMakeFiles/sqliterepo.dir/election.c.o
cd /build/openio-sds-4.1.13/obj-arm-linux-gnueabihf/sqliterepo && /usr/bin/arm-linux-gnueabihf-gcc  -DG_LOG_DOMAIN=\"oio.sqlite\" -DHAVE_ACCEPT4 -DHAVE_SOCKET3 -DHAVE_SOCKLEN_T -DOIOSDS_PROJECT_VERSION=\"master/4.1\" -DTHREADED -D_FILE_OFFSET_BITS=64 -D_GNU_SOURCE -D_LARGEFILE64_SOURCE -D_LARGEFILE_SOURCE -D_LARGE_FILES -D_REENTRANT -Dsqliterepo_EXPORTS -I/build/openio-sds-4.1.13/obj-arm-linux-gnueabihf/sqliterepo -I/build/openio-sds-4.1.13/obj-arm-linux-gnueabihf/metautils/lib -I/build/openio-sds-4.1.13/obj-arm-linux-gnueabihf/metautils/asn1c -I/build/openio-sds-4.1.13/obj-arm-linux-gnueabihf -I/build/openio-sds-4.1.13 -I/build/openio-sds-4.1.13/sqliterepo -I/usr/include/glib-2.0 -I/usr/lib/arm-linux-gnueabihf/glib-2.0/include -I/usr/include/zookeeper  -g -fPIC -pipe -Wall -Wextra -std=gnu99 -Wno-missing-field-initializers -Wunused -Wno-variadic-macros -Wsequence-point -Wredundant-decls -Wshadow -Wcomment -Wmain -Wparentheses -Wfloat-equal -Wunsafe-loop-optimizations -Wunused-but-set-parameter -Wunused-but-set-variable -Wmissing-prototypes -Werror -O2 -fPIC   -o CMakeFiles/sqliterepo.dir/election.c.o   -c /build/openio-sds-4.1.13/sqliterepo/election.c
/build/openio-sds-4.1.13/sqliterepo/election.c: In function 'member_log_event':
/build/openio-sds-4.1.13/sqliterepo/election.c:975:69: error: left shift count >= width of type [-Werror=shift-count-overflow]
  plog->time = (oio_ext_real_time() / G_TIME_SPAN_MILLISECOND) % (1L << 48);
                                                                     ^
/build/openio-sds-4.1.13/sqliterepo/election.c:975:63: error: division by zero [-Werror=div-by-zero]
  plog->time = (oio_ext_real_time() / G_TIME_SPAN_MILLISECOND) % (1L << 48);

Fixes: b17504724c68bc49a1379ddce1ae085cacb4d2fb

sqliterepo/election.c
@@ -972,7 +972,7 @@ member_log_event(struct election_member_s *member, enum election_step_e pre,
 	plog->event = evt;
 	plog->pre = pre;
 	plog->post = member->step;
-	plog->time = (oio_ext_real_time() / G_TIME_SPAN_MILLISECOND) % (1L << 48);
+	plog->time = (oio_ext_real_time() / G_TIME_SPAN_MILLISECOND) % (1LL << 48);
 }
 
 #ifdef HAVE_EXTRA_DEBUG