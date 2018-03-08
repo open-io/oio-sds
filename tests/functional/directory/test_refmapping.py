0865f15fa4de64852e1e46506e212471d94b4661
Aymeric Ducroquetz <aymeric.ducroquetz@openio.io>
2018-03-08 15:19:43 +0100

    oio-meta2-mover: Add an automatic mode to choose the destination

--- tests/functional/directory/test_refmapping.py
+++ tests/functional/directory/test_refmapping.py
@@ -47,7 +47,7 @@ class TestMeta1RefMapping(BaseTestCase):
                 services.append(d['host'])
         return services
 
-    def _test_move(self, service_type, base=None):
+    def _test_move(self, service_type, base=None, destination=True):
         if base is None:
             base = self._get_cid()
         raw_services = self._get_services(service_type)
@@ -57,38 +57,46 @@ class TestMeta1RefMapping(BaseTestCase):
 
         dest_service = None
         all_meta2_services = self.conscience.all_services(service_type, True)
-        for service in all_meta2_services:
-            if service['addr'] not in raw_services:
-                dest_service = service['addr']
-        if dest_service is None:
-            self.skipTest("need at least 2 %s" % service_type)
-        expected_services.append(dest_service)
+        if len(all_meta2_services) <= len(raw_services):
+            self.skipTest("need at least %d %s" % (len(raw_services)+1,
+                                                   service_type))
+        if destination:
+            for service in all_meta2_services:
+                if service['addr'] not in raw_services:
+                    dest_service = service['addr']
+            expected_services.append(dest_service)
 
         moved = self.mapping.move(src_service, dest_service, base,
                                   service_type)
-        self.mapping.apply(moved)
+        moved_ok = self.mapping.apply(moved)
+        self.assertEqual(1, len(moved_ok))
 
         data_dir = self.api.directory.list(self.account, self.reference)
-        meta2_services = list()
+        new_services = list()
         for d in data_dir['srv']:
             if d['type'] == service_type:
-                meta2_services.append(d['host'])
-        self.assertListEqual(sorted(expected_services), sorted(meta2_services))
+                new_services.append(d['host'])
+        if destination:
+            self.assertListEqual(sorted(expected_services),
+                                 sorted(new_services))
+        else:
+            for expected_service in expected_services:
+                self.assertIn(expected_service, new_services)
+            self.assertNotIn(src_service, new_services)
+            self.assertEqual(len(expected_services)+1, len(new_services))
 
-        return expected_services
+        return (src_service, expected_services)
 
     def test_move_meta2(self):
         self.api.container_create(self.account, self.reference)
 
-        expected_services = self._test_move('meta2')
+        _, expected_services = self._test_move('meta2')
 
         properties = self.api.container_get_properties(
             self.account, self.reference)
-        peers = properties['system'].get('sys.peers', None)
-        if peers is not None:
-            new_services = peers.split(',')
-            self.assertListEqual(sorted(expected_services),
-                                 sorted(new_services))
+        peers = properties['system']['sys.peers']
+        new_services = peers.split(',')
+        self.assertListEqual(sorted(expected_services), sorted(new_services))
 
         self.api.object_create(self.account, self.reference,
                                data="move meta2", obj_name="test")
@@ -105,15 +113,40 @@ class TestMeta1RefMapping(BaseTestCase):
             self.account, self.reference)
         base = properties['system']['sys.name']
 
-        expected_services = self._test_move('meta2', base=base)
+        _, expected_services = self._test_move('meta2', base=base)
 
         properties = self.api.container_get_properties(
             self.account, self.reference)
-        peers = properties['system'].get('sys.peers', None)
-        if peers is not None:
-            new_services = peers.split(',')
-            self.assertListEqual(sorted(expected_services),
-                                 sorted(new_services))
+        peers = properties['system']['sys.peers']
+        new_services = peers.split(',')
+        self.assertListEqual(sorted(expected_services), sorted(new_services))
+
+        self.api.object_create(self.account, self.reference,
+                               data="move meta2", obj_name="test")
+        for i in range(0, 10):
+            self.api.object_show(self.account, self.reference, "test")
+
+        self.api.object_delete(self.account, self.reference, "test")
+        sleep(0.5)
+        self.api.container_delete(self.account, self.reference)
+
+    def test_move_meta2_without_destination(self):
+        self.api.container_create(self.account, self.reference)
+        properties = self.api.container_get_properties(
+            self.account, self.reference)
+        base = properties['system']['sys.name']
+
+        src_service, expected_services = self._test_move('meta2', base=base,
+                                                         destination=False)
+
+        properties = self.api.container_get_properties(
+            self.account, self.reference)
+        peers = properties['system']['sys.peers']
+        new_services = peers.split(',')
+        for expected_service in expected_services:
+                self.assertIn(expected_service, new_services)
+        self.assertNotIn(src_service, new_services)
+        self.assertEqual(len(expected_services)+1, len(new_services))
 
         self.api.object_create(self.account, self.reference,
                                data="move meta2", obj_name="test")
