34cbe0931bdc63d2b076be26bdcdcf8823b22eaf
Vincent Auclair <vauclair@online.net>
2018-03-30 11:15:17 +0200

    Following the documentation http://docs.gunicorn.org/en/stable/settings.html The `threads` configuration only works for Gthread worker type and the cose uses evenlet. So move the define to `workers`.
    (cherry picked from commit 78c8608de8e0d94296bfd2246f50e22af2815200)

--- oio/common/wsgi.py
+++ oio/common/wsgi.py
@@ -37,8 +37,7 @@ class Application(BaseApplication):
                           self.conf.get('bind_port', '8000'))
         self.cfg.set('bind', bind)
         self.cfg.set('backlog', self.conf.get('backlog', 2048))
-        self.cfg.set('workers', self.conf.get('workers', 1))
-        self.cfg.set('threads', self.conf.get('threads', CPU_COUNT))
+        self.cfg.set('workers', self.conf.get('workers', CPU_COUNT))
         self.cfg.set('worker_class', self.conf.get('worker_class', 'eventlet'))
         self.cfg.set('worker_connections', self.conf.get(
             'worker_connections', 1000))
