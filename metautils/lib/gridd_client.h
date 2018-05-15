93e2c2ed39acfc305ef4650527037edf2ef1eb0e
Jean-Francois Smigielski <jf.smigielski@gmail.com>
2018-05-15 23:02:00 +0200

    metautils/gridd_client: Restore forgotten comments
    Thanks to @fvennetier

--- metautils/lib/gridd_client.h
+++ metautils/lib/gridd_client.h
@@ -37,23 +37,53 @@ enum client_interest_e
 /* Return TRUE to notify the reply management failed. */
 typedef gboolean (*client_on_reply)(gpointer ctx, MESSAGE reply);
 
-// wrappers to the call to the vtable.
-
+/* Destroy the gridd_client pointed by `self` and free all the linked memory */
 void gridd_client_free (struct gridd_client_s *self);
+
+/* Connect the gridd_client_s `self`to the network endpoint `u` (TCP) */
 GError * gridd_client_connect_url (struct gridd_client_s *self, const gchar *u);
+
+/* Configure the gridd_client_s `self` to send the request in
+ * `req` and call `cb` upon each reply */
 GError * gridd_client_request (struct gridd_client_s *self, GByteArray *req,
 		gpointer ctx, client_on_reply cb);
+
+/* Returns a copy of the last error that occured. */
 GError * gridd_client_error (struct gridd_client_s *self);
+
+/* Tells which among client_interest_e is to be monitored */
 int gridd_client_interest (struct gridd_client_s *self);
+
+/* Returns the last URL the gridd_client connected to */
 const gchar * gridd_client_url (struct gridd_client_s *self);
+
+/* Returns the file descriptor currently used by `self` */
 int gridd_client_fd (struct gridd_client_s *self);
+
+/* Force the file descriptor in `fd` to be used by `self` */
 GError * gridd_client_set_fd(struct gridd_client_s *self, int fd);
+
+/* Force the global timeout for the operation (all the request, including
+ * the redirections */
 void gridd_client_set_timeout (struct gridd_client_s *self, gdouble seconds);
+
+/* Force the connection timeout for each unit request */
 void gridd_client_set_timeout_cnx (struct gridd_client_s *self, gdouble sec);
+
+/* Returns if the client's last change is older than 'now' */
 gboolean gridd_client_expired(struct gridd_client_s *self, gint64 now);
+
+/* Returns FALSE if the client is still expecting events */
 gboolean gridd_client_finished (struct gridd_client_s *self);
+
+/* Initiate the request */
 gboolean gridd_client_start (struct gridd_client_s *self);
+
+/* If expired() is true, sets the internal error and mark the client
+ * as failed */
 gboolean gridd_client_expire (struct gridd_client_s *self, gint64 now);
+
+/* Manage the events raised */
 void gridd_client_react (struct gridd_client_s *self);
 
 /* Gives the ownership of `why` to the gridd_client_s `self` */
