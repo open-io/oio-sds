6174a92f7fad2d33d926880aea2522eabbbcbb66
Ayoub Boudhar <a.boudhar@outlook.com>
2018-10-30 18:14:25 +0100

    Add tests to inspect generated events on container ops

---
+++ tests/functional/event/test_events_emission.py
@@ -0,0 +1,174 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018 OpenIO SAS, as part of OpenIO SDS
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3.0 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.
+
+import json
+import time
+import os
+from random import choice
+from subprocess import check_call
+from oio.api.object_storage import ObjectStorageApi
+from oio.event.beanstalk import Beanstalk, ResponseError
+from oio.event.consumer import DEFAULT_TUBE, ACCOUNT_SERVICE, EventTypes
+from oio.common.utils import cid_from_name
+from oio.container.client import ContainerClient
+from tests.utils import BaseTestCase, random_id
+
+
+class TestMeta2EventsEmission(BaseTestCase):
+    def setUp(self):
+        super(TestMeta2EventsEmission, self).setUp()
+        self.account = self.conf['account']
+        self.namespace = self.conf['namespace']
+        self.container_name = 'TestEventsEmission%f' % time.time()
+        self.container_id = cid_from_name(self.account, self.container_name)
+        self.container_client = ContainerClient(self.conf)
+        self.storage_api = ObjectStorageApi(self.conf['namespace'])
+        self.event_agent_name = 'event-agent-1'
+        self.bt_connections = []
+        self._bt_make_connections(self.conf['services']['beanstalkd'])
+
+    def _bt_make_connections(self, bt_list):
+        for bt_entry in bt_list:
+            self.bt_connections.append(
+                Beanstalk.from_url('beanstalk://{0}'.format(bt_entry['addr'])))
+
+    def _bt_watch(self, tube):
+        for bt in self.bt_connections:
+            bt.watch(tube)
+
+    def _bt_pull_events_by_type(self, type):
+        pulled_events = []
+
+        for bt in self.bt_connections:
+            job_id = True
+            while job_id is not None:
+                try:
+                    job_id, data_raw = bt.reserve(timeout=4)
+                    pulled_events.append(json.loads(data_raw))
+                    bt.delete(job_id)
+                except ResponseError:
+                    break
+
+        return list(
+            filter(lambda x: x.get("event") == type,
+                   pulled_events))
+
+    def _service(self, name, action):
+        name = "%s-%s" % (self.conf['namespace'], name)
+        check_call(['gridinit_cmd', '-S',
+                    os.path.expanduser('~/.oio/sds/run/gridinit.sock'),
+                    action, name])
+        """
+        The current gridinit version that's running on travis returns before
+        the child process terminates. As such we have to introduce an 
+        artificial pause to account for the child process termination. Remove 
+        this once gridinit is upgraded.
+        """
+        time.sleep(3)
+
+    def test_container_create(self):
+        # First shutdown the event-agent
+        self._service(self.event_agent_name, 'stop')
+        self._bt_watch(DEFAULT_TUBE)
+
+        # Fire up the event
+        self.container_client.container_create(self.account,
+                                               self.container_name)
+
+        # Grab all events and filter for the needed event type
+        wanted_events = self._bt_pull_events_by_type(
+            EventTypes.ACCOUNT_SERVICES)
+
+        self.assertEqual(len(wanted_events), 1)
+        # Prepping for the next operation.
+        ev = wanted_events[0]
+
+        # Basic info
+        self.assertEqual(ev.get("url"), {
+            'ns': self.namespace,
+            'account': self.account,
+            'user': self.container_name,
+            'id': self.container_id,
+            'type': 'meta2'
+        })
+
+        # Get the peers list and verify it's the same as received
+        raw_dir_info = self.storage_api.directory.list(self.account,
+                                                       self.container_name,
+                                                       cid=self.container_id)
+        raw_dir_info = raw_dir_info['srv']
+        expected_peers_list = list(
+            filter(lambda x: x.get('type') == 'meta2', raw_dir_info))
+        expected_peers_list = list(
+            map(lambda x: x.get('host'), expected_peers_list))
+
+        received_peers_list = list(
+            filter(lambda x: x.get('type') == 'meta2',
+                   ev.get('data')))
+        received_peers_list = list(
+            map(lambda x: x.get('host'), received_peers_list))
+
+        self.assertListEqual(received_peers_list, expected_peers_list)
+        self._service(self.event_agent_name, 'restart')
+
+    def test_container_delete(self):
+        self._service(self.event_agent_name, 'stop')
+        self._bt_watch(DEFAULT_TUBE)
+
+        # Create the container first
+        self.container_client.container_create(self.account,
+                                               self.container_name)
+
+        # Get the peers list and verify it's the same as received
+        raw_dir_info = self.storage_api.directory.list(self.account,
+                                                       self.container_name,
+                                                       cid=self.container_id)
+        raw_dir_info = raw_dir_info['srv']
+        expected_peers_list = list(
+            filter(lambda x: x.get('type') == 'meta2', raw_dir_info))
+        expected_peers_list = sorted(list(
+            map(lambda x: x.get('host'), expected_peers_list)))
+
+        # Fire up the event
+        self.container_client.container_delete(self.account,
+                                               self.container_name)
+
+        # Sleep to wait for sqliterepo propagation
+        time.sleep(5)
+        # Grab all events and filter for the needed event type
+        wanted_events = self._bt_pull_events_by_type(
+            EventTypes.CONTAINER_DELETED)
+
+        self.assertEqual(len(wanted_events), len(expected_peers_list))
+        # Prepping for the next operation.
+
+        # Basic info
+        for ev in wanted_events:
+            self.assertEqual(ev.get("url"), {
+                'ns': self.namespace,
+                'account': self.account,
+                'user': self.container_name,
+                'id': self.container_id,
+            })
+
+        received_peers = sorted(list(map(
+            lambda x: str(x.get("data").get("peers")[0]),
+            wanted_events
+        )))
+
+        self.assertListEqual(received_peers, expected_peers_list)
+        self._service(self.event_agent_name, 'restart')
