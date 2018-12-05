58cdf80f9f4829816bb7821b20eb89fcdb144354
Florent Vennetier (OpenIO) <florent@fridu.net>
2018-12-05 16:59:31 +0100

    tests: put a deadline on the service score wait loop

--- tests/functional/api/test_objectstorage.py
+++ tests/functional/api/test_objectstorage.py
@@ -306,24 +306,25 @@ class TestObjectStorageApi(ObjectStorageApiTestBase):
         data = self._get_properties(name)
         self.assertEqual(data['properties'], metadata)
 
-    def _wait_account_meta2(self):
+    def _wait_account_meta2(self, timeout=20.0, score_threshold=35):
         # give account and meta2 time to catch their breath
         wait = False
-        while True:
+        deadline = time.time() + timeout
+        while time.time() < deadline:
             try:
                 for service in self.conscience.all_services("account"):
-                    if int(service['score']) < 35:
+                    if int(service['score']) < score_threshold:
                         wait = True
-                        continue
+                        break
                 if not wait:
                     for service in self.conscience.all_services("meta2"):
-                        if int(service['score']) < 35:
+                        if int(service['score']) < score_threshold:
                             wait = True
-                            continue
+                            break
                     if not wait:
                         return
-            except exc.OioException:
-                pass
+            except exc.OioException as err:
+                logging.warn('Could not check service score: %s', err)
             wait = False
             time.sleep(1)
 
