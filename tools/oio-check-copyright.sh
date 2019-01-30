1b4c72a974836235d6cab1276d0481deca6384ca
Florent Vennetier (OpenIO) <florent@fridu.net>
2019-01-30 11:36:02 +0100

    CI: improve copyright check script

--- tools/oio-check-copyright.sh
+++ tools/oio-check-copyright.sh
@@ -9,7 +9,7 @@ BASEDIR=$1
 | grep -i -v -e '^\.' -e '^build' -e '^cmake' -e '^setup' \
 | while read D ; do
 	/usr/bin/find "${BASEDIR}/${D}" -type f \
-		-name '*.h' -or -name '*.c' -or -name '*.py' \
+		-name '*.h' -or -name '*.c' -or -name '*.py' -or -name '*.go' \
 	| while read F ; do
 		if ! [[ -s "$F" ]] ; then continue ; fi
 		if ! /usr/bin/git ls-files --error-unmatch "$F" ; then continue ; fi
@@ -20,3 +20,23 @@ BASEDIR=$1
 	done
 done
 
+if [ -n "$TRAVIS_COMMIT_RANGE" ]
+then
+	INCLUDE='.+\.(c|go|h|py)$'
+	YEAR=$(date +%Y)
+	FAIL=0
+	FILES=$(git diff --name-only "$TRAVIS_COMMIT_RANGE" | grep -E "$INCLUDE")
+	echo "Checking copyright for year $YEAR."
+	for name in $FILES
+	do
+		COPYRIGHT_LINE=$(grep -E 'Copyright.+[[:digit:]]{4}.+OpenIO' "$name")
+		if [[ ! "$COPYRIGHT_LINE" =~ .+$YEAR.* ]]
+		then
+			echo "File $name has just been modified ($YEAR),"
+			echo "but copyright is '$COPYRIGHT_LINE'."
+			FAIL=1
+		fi
+	done
+	exit $FAIL
+	echo "OK"
+fi
