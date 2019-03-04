c4451187ccf19cfbcc22ff9a00909bd345cbb61b
Jean-Francois Smigielski <jf.smigielski@gmail.com>
2019-03-04 21:50:27 +0100

    sqliterepo/election: Avoid blocking the FSM on disconnections
    We should always call the transition() function on an event.

--- sqliterepo/election.c
+++ sqliterepo/election.c
@@ -1636,40 +1636,42 @@ static void
 completion_LISTING(int zrc, const struct String_vector *sv, const void *d)
 {
 	if (!d) return;
-	if (!sv) return;
 
 	struct election_member_s *member = (struct election_member_s*) d;
 	gboolean has_first = FALSE;
 	gint32 first = -1;
-	GArray *i32v = nodev_to_int32v(sv, member->key);
+
+	if (sv) {
+		GArray *i32v = nodev_to_int32v(sv, member->key);
 #ifdef HAVE_ENBUG
-	GRID_WARN("2Master? %s vs %s", sqliterepo_election_2master_db, member->inline_name.base);
-	if (sqliterepo_election_2master_db[0] && !g_ascii_strcasecmp(
-				member->inline_name.base, sqliterepo_election_2master_db)) {
-		/* We try to fake the double-master condition */
-		const gint32 local = member->local_id;
-		GRID_WARN("Yes! local %u len %u", local, i32v->len);
-		if (i32v->len > 1 && g_array_index(i32v, gint32, 0) != local) {
-			/* If the current base is 2nd, we make it think it is first */
-			if (g_array_index(i32v, gint32, 1) == local) {
-				g_array_remove_index(i32v, 0);
+		GRID_WARN("2Master? %s vs %s",
+				sqliterepo_election_2master_db, member->inline_name.base);
+		if (sqliterepo_election_2master_db[0] && !g_ascii_strcasecmp(
+					member->inline_name.base, sqliterepo_election_2master_db)) {
+			/* We try to fake the double-master condition */
+			const gint32 local = member->local_id;
+			GRID_WARN("Yes! local %u len %u", local, i32v->len);
+			if (i32v->len > 1 && g_array_index(i32v, gint32, 0) != local) {
+				/* If the current base is 2nd, we make it think it is first */
+				if (g_array_index(i32v, gint32, 1) == local) {
+					g_array_remove_index(i32v, 0);
+				}
 			}
+			/* avoid looping and do the trick just once, on only one member */
+			sqliterepo_election_2master_db[0] = '\0';
 		}
-		/* avoid looping and do the trick just once, on only one member */
-		sqliterepo_election_2master_db[0] = '\0';
-	}
 #endif  /* HAVE_ENBUG */
-	if (i32v->len > 0) {
-		first = g_array_index(i32v, gint32, 0);
-		has_first = TRUE;
+		if (i32v->len > 0) {
+			first = g_array_index(i32v, gint32, 0);
+			has_first = TRUE;
+		}
+		g_array_free(i32v, TRUE);
 	}
-	g_array_free(i32v, TRUE);
 
 	struct exec_later_LISTING_context_s *ctx =
 		g_slice_alloc0(sizeof(struct exec_later_LISTING_context_s));
 	ctx->magic = DAT_LISTING;
-	if (ZOK == (ctx->zrc = zrc))
-		ctx->zrc = has_first ? ZOK : ZNONODE;
+	ctx->zrc = (zrc == ZOK) ? (has_first ? ZOK : ZNONODE) : zrc;
 	ctx->member = member;
 	ctx->master_id = first;
 
