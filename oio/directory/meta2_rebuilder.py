0b23e4849bac7eefcfb69f507ea476e8ed6e3521
Aymeric Ducroquetz <aymeric.ducroquetz@openio.io>
2019-04-24 12:56:04 +0200

    Python API: Create tool to rebuild meta2

--- oio/directory/meta2_rebuilder.py
+++ oio/directory/meta2_rebuilder.py
@@ -16,40 +16,57 @@
 
 from datetime import datetime
 
+from oio.common.easy_value import int_value
 from oio.common.tool import Tool, ToolWorker
-from oio.common.utils import cid_from_name
-from oio.container.client import ContainerClient
+from oio.rdir.client import RdirClient
 from oio.directory.admin import AdminClient
 from oio.directory.meta2 import Meta2Database
 
 
-class ContainerRepairer(Tool):
+class Meta2Rebuilder(Tool):
     """
-    Repair containers.
+    Rebuild meta2 databases.
     """
 
-    def __init__(self, conf, containers=None, **kwargs):
-        super(ContainerRepairer, self).__init__(conf, **kwargs)
+    DEFAULT_RDIR_FETCH_LIMIT = 100
+
+    def __init__(self, conf, input_file=None, service_id=None, **kwargs):
+        super(Meta2Rebuilder, self).__init__(conf, **kwargs)
 
         # input
-        self.containers = containers
+        self.input_file = input_file
+        self.meta2_id = service_id
+
+        # rawx/rdir
+        self.rdir_client = RdirClient(self.conf, logger=self.logger)
+        self.rdir_fetch_limit = int_value(
+            self.conf.get('rdir_fetch_limit'), self.DEFAULT_RDIR_FETCH_LIMIT)
 
     @staticmethod
     def string_from_item(item):
-        namespace, account, container = item
-        return '%s|%s|%s' % (
-            namespace, account, container)
+        namespace, container_id = item
+        return '%s|%s' % (namespace, container_id)
 
-    def _fetch_items_from_containers(self):
-        for obj in self.containers:
-            namespace = obj['namespace']
-            account = obj['account']
-            container = obj['container']
-            yield namespace, account, container
+    def _fetch_items_from_input_file(self):
+        with open(self.input_file, 'r') as ifile:
+            for line in ifile:
+                stripped = line.strip()
+                if not stripped or stripped.startswith('#'):
+                    continue
+
+                container_id = stripped
+                yield self.namespace, container_id
+
+    def _fetch_items_from_meta2_id(self):
+        containers = self.rdir_client.meta2_index_fetch_all(self.meta2_id)
+        for container in containers:
+            yield self.namespace, container['container_id']
 
     def _fetch_items(self):
-        if self.containers:
-            return self._fetch_items_from_containers()
+        if self.input_file:
+            return self._fetch_items_from_input_file()
+        if self.meta2_id:
+            return self._fetch_items_from_meta2_id()
 
         def _empty_generator():
             return
@@ -57,86 +74,81 @@ class ContainerRepairer(Tool):
         return _empty_generator()
 
     def _get_report(self, status, end_time, counters):
-        containers_processed, total_containers_processed, \
+        references_processed, total_references_processed, \
             errors, total_errors = counters
         time_since_last_report = (end_time - self.last_report) or 0.00001
         total_time = (end_time - self.start_time) or 0.00001
         report = (
             '%(status)s '
             'last_report=%(last_report)s %(time_since_last_report).2fs '
-            'containers=%(containers)d %(containers_rate).2f/s '
+            'references=%(references)d %(references_rate).2f/s '
             'errors=%(errors)d %(errors_rate).2f%% '
             'start_time=%(start_time)s %(total_time).2fs '
-            'total_containers='
-            '%(total_containers)d %(total_containers_rate).2f/s '
+            'total_references='
+            '%(total_references)d %(total_references_rate).2f/s '
             'total_errors=%(total_errors)d %(total_errors_rate).2f%%' % {
                 'status': status,
                 'last_report': datetime.fromtimestamp(
                     int(self.last_report)).isoformat(),
                 'time_since_last_report': time_since_last_report,
-                'containers': containers_processed,
-                'containers_rate':
-                    containers_processed / time_since_last_report,
+                'references': references_processed,
+                'references_rate':
+                    references_processed / time_since_last_report,
                 'errors': errors,
-                'errors_rate': 100 * errors / float(containers_processed or 1),
+                'errors_rate': 100 * errors / float(references_processed or 1),
                 'start_time': datetime.fromtimestamp(
                     int(self.start_time)).isoformat(),
                 'total_time': total_time,
-                'total_containers': total_containers_processed,
-                'total_containers_rate':
-                    total_containers_processed / total_time,
+                'total_references': total_references_processed,
+                'total_references_rate':
+                    total_references_processed / total_time,
                 'total_errors': total_errors,
                 'total_errors_rate':
-                    100 * total_errors / float(total_containers_processed or 1)
+                    100 * total_errors / float(total_references_processed or 1)
                 })
         if self.total_expected_items is not None:
-            progress = 100 * total_containers_processed / \
+            progress = 100 * total_references_processed / \
                 float(self.total_expected_items or 1)
             report += ' progress=%d/%d %.2f%%' % \
-                (total_containers_processed, self.total_expected_items,
+                (total_references_processed, self.total_expected_items,
                  progress)
         return report
 
     def create_worker(self, queue_workers, queue_reply):
-        return ContainerRepairerWorker(self, queue_workers, queue_reply)
+        return ContentRepairerWorker(self, queue_workers, queue_reply)
 
     def _load_total_expected_items(self):
-        if self.containers and isinstance(self.containers, list):
-            self.total_expected_items = len(self.containers)
+        pass
 
 
-class ContainerRepairerWorker(ToolWorker):
+class ContentRepairerWorker(ToolWorker):
 
     def __init__(self, tool, queue_workers, queue_reply):
-        super(ContainerRepairerWorker, self).__init__(
+        super(ContentRepairerWorker, self).__init__(
             tool, queue_workers, queue_reply)
 
         self.admin_client = AdminClient(self.conf, logger=self.logger)
-        self.container_client = ContainerClient(self.conf, logger=self.logger)
         self.meta2_database = Meta2Database(self.conf, logger=self.logger)
 
     def _process_item(self, item):
-        namespace, account, container = item
+        namespace, container_id = item
         if namespace != self.tool.namespace:
             raise ValueError('Invalid namespace (actual=%s, expected=%s)' % (
                 namespace, self.tool.namespace))
 
+        self.logger.debug('Rebuilding %s', self.tool.string_from_item(item))
         errors = list()
-        cid = cid_from_name(account, container)
-        for res in self.meta2_database.rebuild(cid):
+        for res in self.meta2_database.rebuild(container_id):
             if res['err']:
                 errors.append('%s: %s' % (res['base'], res['err']))
         if errors:
             raise Exception(errors)
 
         data = self.admin_client.election_sync(
-            service_type='meta2', account=account, reference=container)
+            service_type='meta2', cid=container_id)
         for host, info in data.items():
             if info['status']['status'] not in (200, 301):
                 errors.append('%s (%d): %s' % (
                     host, info['status']['status'], info['status']['message']))
         if errors:
             raise Exception(errors)
-
-        self.container_client.container_touch(
-            account=account, reference=container)
