443b5bc6afbe5a4044e5512576d62f25df44aa1f
Michael Bonfils <bonfils.michael@protonmail.com>
2019-06-17 11:34:09 +0200

    Update delete_many with force-versioning

--- tests/functional/container/test_container.py
+++ tests/functional/container/test_container.py
@@ -724,6 +724,69 @@ class TestMeta2Containers(BaseTestCase):
         self.assertEqual(2, len(data['objects']))
         self.assertTrue(data['objects'][0]['deleted'])
 
+    def test_object_with_versioning_header_with_delete_many(self):
+        params = self.param_ref(self.ref)
+        objs = []
+
+        for _ in range(10):
+            path = random_content()
+            objs.append(path)
+            self._create_content(path)
+
+        resp = self.request('POST', self.url_container('get_properties'),
+                            params=params)
+        data = json.loads(resp.data)
+        self.assertNotIn("sys.m2.policy.version", data['system'].keys())
+
+        # delete two objects without header
+        data = {'contents': [{'name': objs.pop()}, {'name': objs.pop()}]}
+        resp = self.request('POST', self.url_content('delete_many'),
+                            params=params, data=json.dumps(data))
+        resp = self.request('POST', self.url_container('get_properties'),
+                            params=params)
+        data = json.loads(resp.data)
+        self.assertNotIn("sys.m2.policy.version", data['system'].keys())
+        self.assertEqual("8", data['system'].get('sys.m2.objects', -1))
+
+        # delete two objects with header enabling versioning
+        data = {'contents': [{'name': objs.pop()}, {'name': objs.pop()}]}
+        resp = self.request('POST', self.url_content('delete_many'),
+                            params=params, data=json.dumps(data),
+                            headers={FORCEVERSIONING_HEADER: -1})
+        self.assertEqual(resp.status, 200)
+
+        resp = self.request('POST', self.url_container('get_properties'),
+                            params=params)
+        data = json.loads(resp.data)
+        self.assertEqual("-1", data['system'].get("sys.m2.policy.version", 0))
+
+        resp = self.request('GET', self.url_container('list'),
+                            params=merge(params, {'all': 1}))
+        data = json.loads(resp.data)
+        self.assertEqual(10, len(data['objects']))
+
+        resp = self.request('GET', self.url_container('list'),
+                            params=params)
+        data = json.loads(resp.data)
+        self.assertEqual(6, len(data['objects']))
+
+        # delete two objects with header disabling versioning
+        data = {'contents': [{'name': objs.pop()}, {'name': objs.pop()}]}
+        resp = self.request('POST', self.url_content('delete_many'),
+                            params=params, data=json.dumps(data),
+                            headers={FORCEVERSIONING_HEADER: 1})
+        self.assertEqual(resp.status, 200)
+
+        resp = self.request('POST', self.url_container('get_properties'),
+                            params=params)
+        data = json.loads(resp.data)
+        self.assertEqual("1", data['system'].get("sys.m2.policy.version", 0))
+
+        resp = self.request('GET', self.url_container('list'),
+                            params=merge(params, {'all': 1}))
+        data = json.loads(resp.data)
+        self.assertEqual(8, len(data['objects']))
+
 
 class TestMeta2Contents(BaseTestCase):
     def setUp(self):
