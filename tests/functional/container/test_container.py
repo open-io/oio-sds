efa85dc09e212911832d49262a4ca2a022dae3ad
Jean-Francois Smigielski <jf.smigielski@gmail.com>
2019-07-24 10:55:00 +0200

    tests: Fix the tests around SPARE chunks
    Adapt them to the latest evolution of prepare/spare chunks

--- tests/functional/container/test_container.py
+++ tests/functional/container/test_container.py
@@ -1036,18 +1036,13 @@ class TestMeta2Contents(BaseTestCase):
         return True
 
     def test_prepare(self):
-        headers = {'X-oio-action-mode': 'autocreate'}
         params = self.param_content(self.ref, random_content())
 
         resp = self.request('POST', self.url_content('prepare'), params=params)
         self.assertError(resp, 400, 400)
+        # A content/prepare now works despite the container is not created
         resp = self.request('POST', self.url_content('prepare'),
                             params=params, data=json.dumps({'size': 1024}))
-        self.assertError(resp, 404, 406)
-        resp = self.request('POST', self.url_content('prepare'),
-                            params=params, data=json.dumps({'size': 1024}),
-                            headers=headers)
-        self.assertEqual(resp.status, 200)
         self.assertTrue(self.valid_chunks(self.json_loads(resp.data)))
         # TODO test /content/prepare with additional useless parameters
         # TODO test /content/prepare with invalid sizes
@@ -1069,8 +1064,11 @@ class TestMeta2Contents(BaseTestCase):
         self.assertEqual(resp.status, 400)
 
     def test_spare_with_one_missing(self):
+        stg_policy = self.conscience.info().get(
+                'options', dict()).get('storage_policy')
         headers = {'X-oio-action-mode': 'autocreate'}
         params = self.param_content(self.ref, random_content())
+        params.update({'stgpol': stg_policy})
         resp = self.request('POST', self.url_content('prepare2'),
                             params=params, data=json.dumps({'size': 1024}),
                             headers=headers)
@@ -1094,8 +1092,12 @@ class TestMeta2Contents(BaseTestCase):
         self.assertEqual(1, len(spare_data['properties']))
 
     def _test_spare_with_n_broken(self, count_broken):
+        stg_policy = self.conscience.info().get(
+                'options', dict()).get('storage_policy')
+
         headers = {'X-oio-action-mode': 'autocreate'}
         params = self.param_content(self.ref, random_content())
+        params.update({'stgpol': stg_policy})
         resp = self.request('POST', self.url_content('prepare2'),
                             params=params, data=json.dumps({'size': 1024}),
                             headers=headers)
@@ -1119,6 +1121,7 @@ class TestMeta2Contents(BaseTestCase):
                             data=json.dumps(
                                 {"notin": chunks,
                                  "broken": broken}))
+        print resp.data
         self.assertEqual(resp.status, 200)
         spare_data = self.json_loads(resp.data)
         # Since we extracted N chunks, there must be exactly N chunks in
