3c014e28e867c1cd212e4c73f7f3b8c38dad9783
Aymeric Ducroquetz <aymeric.ducroquetz@openio.io>
2019-09-23 16:03:52 +0200

    Allow to set various Redis parameters

--- oio/common/redis_conn.py
+++ oio/common/redis_conn.py
@@ -16,29 +16,67 @@
 from time import time, sleep
 import uuid
 import math
+import importlib
 
-import redis
-import redis.sentinel
+from oio.common.easy_value import true_value
 
 
-class RedisConn(object):
+class RedisConnection(object):
 
-    def __init__(self, conf, connection=None, **kwargs):
-        self.conf = conf
-        self._conn = connection
+    # Imported from redis-py, for compatibility with pre 2.10.6 versions.
+    URL_QUERY_ARGUMENT_PARSERS = {
+        'socket_timeout': float,
+        'socket_connect_timeout': float,
+        'socket_keepalive': true_value,
+        'retry_on_timeout': true_value,
+        'max_connections': int,
+        'health_check_interval': int,
+    }
+
+    def __init__(self, host=None, sentinel_hosts=None,
+                 sentinel_name=None, **kwargs):
+        self.__redis_mod = importlib.import_module('redis')
+        self.__redis_sentinel_mod = importlib.import_module('redis.sentinel')
+
+        self._conn = None
+        self._host = None
+        self._port = None
         self._sentinel = None
-        self._sentinel_hosts = conf.get('sentinel_hosts', None)
-        self._sentinel_name = conf.get('sentinel_master_name', 'oio')
+        self._sentinel_hosts = None
+        self._sentinel_name = None
+        self._conn_kwargs = self._filter_conn_kwargs(kwargs)
 
-        # Do not use Sentinel if a connection object is provided
-        if self._sentinel_hosts and not self._conn:
-            self._sentinel = redis.sentinel.Sentinel(
-                    [(h, int(p)) for h, p, in (hp.split(':', 2)
-                     for hp in self._sentinel_hosts.split(','))])
+        if host:
+            self._host, self._port = host.rsplit(':', 1)
+            self._port = int(self._port)
+            return
 
-    def register_script(self, script):
-        """Register a LUA script and return Script object."""
-        return self.conn.register_script(script)
+        if not sentinel_name:
+            raise ValueError("missing parameter 'sentinel_name'")
+
+        if isinstance(sentinel_hosts, basestring):
+            sentinel_hosts = sentinel_hosts.split(',')
+        self._sentinel_hosts = [(h, int(p)) for h, p, in (hp.rsplit(':', 1)
+                                for hp in sentinel_hosts)]
+        self._sentinel_name = sentinel_name
+        self._sentinel = self.__redis_sentinel_mod.Sentinel(
+            self._sentinel_hosts,
+            **self._conn_kwargs)
+
+    def _filter_conn_kwargs(self, conn_kwargs):
+        """
+        Keep only keyword arguments known by Redis classes, cast them to
+        the appropriate type.
+        """
+        if conn_kwargs is None:
+            return None
+        if hasattr(self.__redis_mod.connection, 'URL_QUERY_ARGUMENT_PARSERS'):
+            parsers = self.__redis_mod.connection.URL_QUERY_ARGUMENT_PARSERS
+        else:
+            parsers = self.URL_QUERY_ARGUMENT_PARSERS
+        return {k: parsers[k](v)
+                for k, v in conn_kwargs.items()
+                if k in parsers}
 
     @property
     def conn(self):
@@ -46,10 +84,21 @@ class RedisConn(object):
         if self._sentinel:
             return self._sentinel.master_for(self._sentinel_name)
         if not self._conn:
-            redis_host = self.conf.get('redis_host', '127.0.0.1')
-            redis_port = int(self.conf.get('redis_port', '6379'))
-            self._conn = redis.StrictRedis(host=redis_host, port=redis_port)
+            self._conn = self.__redis_mod.StrictRedis(
+                host=self._host, port=self._port,
+                **self._conn_kwargs)
         return self._conn
+
+    @property
+    def conn_slave(self):
+        """Retrieve Redis connection (normal or sentinel)"""
+        if self._sentinel:
+            return self._sentinel.slave_for(self._sentinel_name)
+        return self.conn
+
+    def register_script(self, script):
+        """Register a LUA script and return Script object."""
+        return self.conn.register_script(script)
 
     def acquire_lock_with_timeout(self, lockname, acquire_timeout=10,
                                   lock_timeout=10):
@@ -88,7 +137,7 @@ class RedisConn(object):
                 pipe.unwatch()
                 break
 
-            except redis.exceptions.WatchError:
+            except self.__redis_mod.exceptions.WatchError:
                 pass
 
         return False
