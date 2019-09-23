98500edcfe68814ae2d7accfeb393e6f32259026
Jean-Francois Smigielski <jf.smigielski@gmail.com>
2019-09-23 13:18:39 +0200

    WIP

--- oio/corba/worker.py
+++
@@ -1,8 +0,0 @@
-#/us/bin/env python
-
-import oio.corba
-
-if __name__ == '__main__':
-    source = oio.corba.BeanstalkdSource("127.0.0.1:6005", "worker")
-    sink = oio.corba.BeanstalkdSink()
-    oio.corba.Worker(source, sink).run()
