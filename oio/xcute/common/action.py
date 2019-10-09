8485bede986bbe43789b37fe907c2b995b6254bb
Aymeric Ducroquetz <aymeric.ducroquetz@openio.io>
2019-10-09 23:44:19 +0200

    xcute: Create a common dispatcher

---
+++ oio/xcute/common/action.py
@@ -0,0 +1,25 @@
+# Copyright (C) 2019 OpenIO SAS, as part of OpenIO SDS
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3.0 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.
+
+
+class XcuteAction(object):
+    """Serialisable wrapper for an action submitted in the xcute hub."""
+
+    def __init__(self, conf, logger):
+        self.conf = conf
+        self.logger = logger
+
+    def process(self, *args, **kwargs):
+        raise NotImplementedError()
