9a4aa4305dd8c05ca0ee5740e4cdf0bbe886e21b
Aymeric Ducroquetz <aymeric.ducroquetz@openio.io>
2019-11-15 22:50:49 +0100

    xcute: Allow to ratelimit the job

--- oio/xcute/common/job.py
+++ oio/xcute/common/job.py
@@ -13,6 +13,8 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library.
 
+import math
+
 from oio.common.easy_value import int_value
 from oio.common.logger import get_logger
 
@@ -21,7 +23,7 @@ class XcuteJob(object):
 
     JOB_TYPE = None
 
-    DEFAULT_TASKS_BATCH_SIZE = 32
+    DEFAULT_TASKS_PER_SECOND = 32
     MAX_TASKS_BATCH_SIZE = 512
 
     def __init__(self, conf, logger=None):
@@ -36,12 +38,22 @@ class XcuteJob(object):
         """
         sanitized_job_config = dict()
 
+        self.tasks_per_second = int_value(
+            job_config.get('tasks_per_second'),
+            self.DEFAULT_TASKS_PER_SECOND)
+        sanitized_job_config['tasks_per_second'] = self.tasks_per_second
+
         self.tasks_batch_size = int_value(
-            job_config.get('tasks_batch_size'),
-            self.DEFAULT_TASKS_BATCH_SIZE)
-        if self.tasks_batch_size < 1:
+            job_config.get('tasks_batch_size'), None)
+        if self.tasks_batch_size is None:
+            if self.tasks_per_second > 0:
+                self.tasks_batch_size = min(
+                    self.tasks_per_second, self.MAX_TASKS_BATCH_SIZE)
+            else:
+                self.tasks_batch_size = self.MAX_TASKS_BATCH_SIZE
+        elif self.tasks_batch_size < 1:
             raise ValueError('Tasks batch size should positive')
-        if self.tasks_batch_size > self.MAX_TASKS_BATCH_SIZE:
+        elif self.tasks_batch_size > self.MAX_TASKS_BATCH_SIZE:
             raise ValueError('Tasks batch size should less than %d' %
                              self.MAX_TASKS_BATCH_SIZE)
         sanitized_job_config['tasks_batch_size'] = self.tasks_batch_size
