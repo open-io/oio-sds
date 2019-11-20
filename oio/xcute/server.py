a558d1b3c520c5fb8d9981590425c8ace2cfc4d0
Aymeric Ducroquetz <aymeric.ducroquetz@openio.io>
2019-11-20 15:23:57 +0100

    xcute: Fix missing Copyright

--- oio/xcute/server.py
+++ oio/xcute/server.py
@@ -1,3 +1,18 @@
+# Copyright (C) 2019 OpenIO SAS, as part of OpenIO SDS
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from werkzeug.exceptions import BadRequest as HTTPBadRequest
 from werkzeug.exceptions import NotFound as HTTPNotFound
 from werkzeug.routing import Map, Rule, Submount
