9d91279018d8ae20f14f7fcf18c561d26886e6f7
Aymeric Ducroquetz <aymeric.ducroquetz@openio.io>
2019-11-25 17:24:15 +0100

    Admin CLI: Prefix all commands with 'xcute' to use 'xcute'

--- oio/cli/admin/xcute/rawx.py
+++ oio/cli/admin/xcute/rawx.py
@@ -15,7 +15,93 @@
 
 from oio.cli import Lister
 from oio.cli.admin.common import MultipleServicesCommandMixin
+from oio.xcute.jobs.blob_rebuilder import RawxRebuildJob
 from oio.xcute.jobs.mover import RawxDecommissionJob
+
+
+class RawxRebuild(MultipleServicesCommandMixin, Lister):
+    """
+    Rebuild chunks that were on the specified volume.
+    It is necessary to declare an incident (with 'openio volume admin
+    incident') before running this command.
+    """
+
+    columns = ('Service ID', 'Job ID')
+
+    @property
+    def logger(self):
+        return self.app.client_manager.logger
+
+    @property
+    def xcute(self):
+        return self.app.client_manager.xcute_client
+
+    def get_parser(self, prog_name):
+        parser = super(RawxRebuild, self).get_parser(prog_name)
+        MultipleServicesCommandMixin.patch_parser(self, parser)
+
+        parser.add_argument(
+            '--chunks-per-second', type=int,
+            help='Max chunks per second. '
+                 '(default=%d)'
+                 % RawxRebuildJob.DEFAULT_TASKS_PER_SECOND)
+        parser.add_argument(
+            '--rdir-fetch-limit', type=int,
+            help='Maximum number of entries returned in each rdir response. '
+                 '(default=%d)'
+            % RawxRebuildJob.DEFAULT_RDIR_FETCH_LIMIT)
+        parser.add_argument(
+            '--rdir-timeout', type=float,
+            help='Timeout for rdir operations, in seconds. (default=%f)'
+                 % RawxRebuildJob.DEFAULT_RDIR_TIMEOUT)
+        parser.add_argument(
+            '--rawx-timeout', type=float,
+            help='Timeout for rawx operations, in seconds. (default=%f)'
+                 % RawxRebuildJob.DEFAULT_RAWX_TIMEOUT)
+        parser.add_argument(
+            '--dry-run', action='store_true',
+            help='Display actions but do nothing.')
+        parser.add_argument(
+            '--delete-faulty-chunks', action='store_true',
+            help='Try to delete faulty chunks after they have been '
+                 'rebuilt elsewhere. This option is useful if the chunks '
+                 'you are rebuilding are not actually missing but are '
+                 'corrupted.')
+        parser.add_argument(
+            '--allow-frozen-container', action='store_true',
+            help='Allow rebuilding a chunk in a frozen container.')
+
+        return parser
+
+    def _take_action(self, parsed_args):
+        job_params = {
+            'rdir_fetch_limit': parsed_args.rdir_fetch_limit,
+            'rdir_timeout': parsed_args.rdir_timeout,
+            'rawx_timeout': parsed_args.rawx_timeout,
+            'dry_run': parsed_args.dry_run,
+            'try_chunk_delete': parsed_args.delete_faulty_chunks,
+            'allow_frozen_container': parsed_args.allow_frozen_container
+        }
+        job_config = {
+            'tasks_per_second': parsed_args.chunks_per_second,
+            'params': job_params
+        }
+
+        for service_id in parsed_args.services:
+            job_params['service_id'] = service_id
+            try:
+                job_info = self.xcute.job_create(
+                    RawxRebuildJob.JOB_TYPE, job_config=job_config)
+                res = job_info['job']['id']
+            except Exception as exc:
+                self.success = False
+                res = str(exc)
+            yield (service_id, res)
+
+    def take_action(self, parsed_args):
+        MultipleServicesCommandMixin.check_and_load_parsed_args(
+            self, self.app, parsed_args)
+        return (self.columns, self._take_action(parsed_args))
 
 
 class RawxDecommission(MultipleServicesCommandMixin, Lister):
