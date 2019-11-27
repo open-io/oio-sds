39655159cd3713c413738ee044759732da13d4e9
Aymeric Ducroquetz <aymeric.ducroquetz@openio.io>
2019-11-27 12:22:05 +0100

    xcute: Keep tasks order to update tasks sent

--- oio/xcute/orchestrator.py
+++ oio/xcute/orchestrator.py
@@ -185,7 +185,9 @@ class XcuteOrchestrator(object):
             tasks_run_time = 0
             batch_per_second = tasks_per_second / float(
                 tasks_batch_size)
-            tasks = dict()
+            # The backend must have the tasks in order
+            # to know the last task sent
+            tasks = OrderedDict()
             for task_id, task_payload in job_tasks:
                 if not self.running:
                     break
