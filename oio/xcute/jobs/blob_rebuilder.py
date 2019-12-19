87f7e8a0aaa016aef45320fec1d6667c2f26cbdf
Aymeric Ducroquetz <aymeric.ducroquetz@openio.io>
2019-12-19 14:24:17 +0100

    xcute: Decrease tasks bacth size for some jobs

--- oio/xcute/jobs/blob_rebuilder.py
+++ oio/xcute/jobs/blob_rebuilder.py
@@ -65,6 +65,7 @@ class RawxRebuildJob(XcuteJob):
     JOB_TYPE = 'rawx-rebuild'
     TASK_CLASS = RawxRebuildTask
 
+    MAX_TASKS_BATCH_SIZE = 128
     DEFAULT_RDIR_FETCH_LIMIT = 1000
     DEFAULT_RDIR_TIMEOUT = 60.0
     DEFAULT_RAWX_TIMEOUT = 60.0
