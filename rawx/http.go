8869c6c1a907c4f0508a8de5588751cc53629b11
Jean-Francois Smigielski <jf.smigielski@gmail.com>
2020-06-24 16:23:48 +0200

    rawx: Fix the Affero header
    An odd mention to Lesser had been left

--- rawx/http.go
+++ rawx/http.go
@@ -11,7 +11,7 @@ package main
 // This library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
+// Affero General Public License for more details.
 //
 // You should have received a copy of the GNU Affero General Public
 // License along with this program. If not, see <http://www.gnu.org/licenses/>.
