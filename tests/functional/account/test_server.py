9c1a793d1f4c0d3c64194951fc856cc1b5979057
Florent Vennetier <florent.vennetier@ovhcloud.com>
2021-01-19 15:38:22 +0100

    account: implement basic IAM tests

--- tests/functional/account/test_server.py
+++ tests/functional/account/test_server.py
@@ -1,4 +1,5 @@
 # Copyright (C) 2015-2019 OpenIO SAS, as part of OpenIO SDS
+# Copyright (C) 2021 OVH SAS
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -17,19 +18,34 @@ import simplejson as json
 
 from werkzeug.test import Client
 from werkzeug.wrappers import BaseResponse
+
 from oio.account.server import create_app
-from tests.utils import BaseTestCase
 from oio.common.timestamp import Timestamp
+from tests.utils import BaseTestCase
 
 
-class TestAccountServer(BaseTestCase):
+class TestAccountServerBase(BaseTestCase):
     def setUp(self):
-        super(TestAccountServer, self).setUp()
+        super(TestAccountServerBase, self).setUp()
         _, _, self.redis_host, self.redis_port = self.get_service('redis')
-        conf = {'redis_host': self.redis_host, 'redis_port': self.redis_port}
+        # Make IAM use the second database so we can flush it between tests
+        iam_cnxstr = 'redis://%s:%s/?db=1&allow_empty_policy_name=False' % (
+            self.redis_host, self.redis_port)
+        conf = {'redis_host': self.redis_host, 'redis_port': self.redis_port,
+                'namespace': self.ns, 'iam.connection': iam_cnxstr}
         self.account_id = 'test'
 
-        self.app = Client(create_app(conf), BaseResponse)
+        self.acct_app = create_app(conf)
+        self.app = Client(self.acct_app, BaseResponse)
+
+
+class TestAccountServer(TestAccountServerBase):
+    """
+    Test account-related features of the account service.
+    """
+
+    def setUp(self):
+        super(TestAccountServer, self).setUp()
         self._create_account()
 
     def _create_account(self):
@@ -156,3 +172,257 @@ class TestAccountServer(BaseTestCase):
                             query_string={'id': self.account_id})
         resp = self.json_loads(resp.data)
         self.assertEqual(len(resp["listing"]), 0)
+
+
+IAM_POLICY_FULLACCESS = """{
+    "Statement": [
+        {
+            "Sid": "FullAccess",
+            "Action": [
+                "s3:*"
+            ],
+            "Effect": "Allow",
+            "Resource": [
+                "*"
+            ]
+        }
+    ]
+}
+"""
+
+
+class TestIamServer(TestAccountServerBase):
+    """
+    Test IAM-related features of the account service.
+    """
+
+    def setUp(self):
+        super(TestIamServer, self).setUp()
+        self.user1 = self.account_id + ':user1'
+        self.user2 = self.account_id + ':user2'
+
+    def tearDown(self):
+        # We do not use the main database but another one,
+        # we can flush it without messing up with other account data.
+        self.acct_app.iam.redis.conn.flushdb()
+        super(TestIamServer, self).tearDown()
+
+    def test_put_user_policy(self):
+        resp = self.app.put('/v1.0/iam/put-user-policy',
+                            query_string={'account': self.account_id,
+                                          'user': self.user1,
+                                          'policy-name': 'mypolicy'},
+                            data=IAM_POLICY_FULLACCESS.encode('utf-8'))
+        self.assertEqual(resp.status_code, 201)
+
+    def test_put_user_policy_no_body(self):
+        resp = self.app.put('/v1.0/iam/put-user-policy',
+                            query_string={'account': self.account_id,
+                                          'user': self.user1,
+                                          'policy-name': 'mypolicy'})
+        self.assertIn(b'Missing policy document', resp.data)
+        self.assertEqual(resp.status_code, 400)
+
+    def test_put_user_policy_no_name(self):
+        resp = self.app.put('/v1.0/iam/put-user-policy',
+                            query_string={'account': self.account_id,
+                                          'user': self.user1},
+                            data=IAM_POLICY_FULLACCESS.encode('utf-8'))
+        self.assertEqual(resp.status_code, 400)
+        self.assertIn(b'policy name cannot be empty', resp.data)
+
+    def test_put_user_policy_invalid_name(self):
+        resp = self.app.put('/v1.0/iam/put-user-policy',
+                            query_string={'account': self.account_id,
+                                          'user': self.user1,
+                                          'policy-name': 'invalid:policy'},
+                            data=IAM_POLICY_FULLACCESS.encode('utf-8'))
+        self.assertIn(b'policy name does not match', resp.data)
+        self.assertEqual(resp.status_code, 400)
+
+    def test_put_user_policy_not_json(self):
+        resp = self.app.put('/v1.0/iam/put-user-policy',
+                            query_string={'account': self.account_id,
+                                          'user': self.user1,
+                                          'policy-name': 'mypolicy'},
+                            data='FullAccess')
+        self.assertIn(b'policy is not JSON-formatted', resp.data)
+        self.assertEqual(resp.status_code, 400)
+
+    def test_put_user_policy_wrong_method(self):
+        resp = self.app.get('/v1.0/iam/put-user-policy',
+                            query_string={'account': self.account_id,
+                                          'user': self.user1,
+                                          'policy-name': 'mypolicy'},
+                            data=IAM_POLICY_FULLACCESS.encode('utf-8'))
+        self.assertEqual(resp.status_code, 405)
+
+    def _compare_policies(self, expected, actual):
+        exp_st = expected.get('Statement', {})
+        act_st = actual.get('Statement', {})
+        self.assertEqual(exp_st[0], act_st[0])
+
+    def test_get_user_policy(self):
+        resp = self.app.put('/v1.0/iam/put-user-policy',
+                            query_string={'account': self.account_id,
+                                          'user': self.user1,
+                                          'policy-name': 'mypolicy'},
+                            data=IAM_POLICY_FULLACCESS.encode('utf-8'))
+        resp = self.app.get('/v1.0/iam/get-user-policy',
+                            query_string={'account': self.account_id,
+                                          'user': self.user1,
+                                          'policy-name': 'mypolicy'})
+        self.assertEqual(resp.status_code, 200)
+        expected = json.loads(IAM_POLICY_FULLACCESS)
+        actual = json.loads(resp.data.decode('utf-8'))
+        self._compare_policies(expected, actual)
+
+    def test_get_user_policy_no_name(self):
+        resp = self.app.get('/v1.0/iam/get-user-policy',
+                            query_string={'account': self.account_id,
+                                          'user': self.user1})
+        # XXX: for backward compatibility reasons, we accept to load
+        # a policy with no name.
+        self.assertIn(b'not found', resp.data)
+        self.assertEqual(resp.status_code, 404)
+
+    def test_get_user_policy_not_existing(self):
+        resp = self.app.get('/v1.0/iam/get-user-policy',
+                            query_string={'account': self.account_id,
+                                          'user': self.user1,
+                                          'policy-name': 'missing'})
+        self.assertIn(b'not found', resp.data)
+        self.assertEqual(resp.status_code, 404)
+
+    def test_list_user_policies(self):
+        # First policy
+        resp = self.app.put('/v1.0/iam/put-user-policy',
+                            query_string={'account': self.account_id,
+                                          'user': self.user1,
+                                          'policy-name': 'mypolicy'},
+                            data=IAM_POLICY_FULLACCESS.encode('utf-8'))
+        self.assertEqual(resp.status_code, 201)
+        resp = self.app.get('/v1.0/iam/list-user-policies',
+                            query_string={'account': self.account_id,
+                                          'user': self.user1})
+        self.assertEqual(resp.status_code, 200)
+        actual = json.loads(resp.data.decode('utf-8'))
+        self.assertIn('PolicyNames', actual)
+        self.assertEqual(actual['PolicyNames'], ['mypolicy'])
+
+        # Second policy
+        resp = self.app.put('/v1.0/iam/put-user-policy',
+                            query_string={'account': self.account_id,
+                                          'user': self.user1,
+                                          'policy-name': 'mysecondpolicy'},
+                            data=IAM_POLICY_FULLACCESS.encode('utf-8'))
+
+        self.assertEqual(resp.status_code, 201)
+        resp = self.app.get('/v1.0/iam/list-user-policies',
+                            query_string={'account': self.account_id,
+                                          'user': self.user1})
+        self.assertEqual(resp.status_code, 200)
+        actual = json.loads(resp.data.decode('utf-8'))
+        self.assertIn('PolicyNames', actual)
+        self.assertEqual(actual['PolicyNames'], ['mypolicy', 'mysecondpolicy'])
+
+    def test_list_user_policies_no_policies(self):
+        resp = self.app.get('/v1.0/iam/list-user-policies',
+                            query_string={'account': self.account_id,
+                                          'user': self.user1})
+        self.assertEqual(resp.status_code, 200)
+        actual = json.loads(resp.data.decode('utf-8'))
+        self.assertIn('PolicyNames', actual)
+        self.assertFalse(actual['PolicyNames'])
+
+    def test_list_users(self):
+        # First user
+        resp = self.app.put('/v1.0/iam/put-user-policy',
+                            query_string={'account': self.account_id,
+                                          'user': self.user1,
+                                          'policy-name': 'mypolicy'},
+                            data=IAM_POLICY_FULLACCESS.encode('utf-8'))
+        self.assertEqual(resp.status_code, 201)
+        resp = self.app.get('/v1.0/iam/list-users',
+                            query_string={'account': self.account_id})
+        self.assertEqual(resp.status_code, 200)
+        actual = json.loads(resp.data.decode('utf-8'))
+        self.assertIn('Users', actual)
+        self.assertEqual(actual['Users'], [self.user1])
+
+        # Second user
+        resp = self.app.put('/v1.0/iam/put-user-policy',
+                            query_string={'account': self.account_id,
+                                          'user': self.user2,
+                                          'policy-name': 'mypolicy'},
+                            data=IAM_POLICY_FULLACCESS.encode('utf-8'))
+        self.assertEqual(resp.status_code, 201)
+        resp = self.app.get('/v1.0/iam/list-users',
+                            query_string={'account': self.account_id})
+        self.assertEqual(resp.status_code, 200)
+        actual = json.loads(resp.data.decode('utf-8'))
+        self.assertIn('Users', actual)
+        self.assertEqual(actual['Users'], [self.user1, self.user2])
+
+    def test_list_users_no_user(self):
+        resp = self.app.get('/v1.0/iam/list-users',
+                            query_string={'account': self.account_id})
+        self.assertEqual(resp.status_code, 200)
+        actual = json.loads(resp.data.decode('utf-8'))
+        self.assertIn('Users', actual)
+        self.assertFalse(actual['Users'])
+
+    def test_delete_user_policy(self):
+        # Put a bunch of policies
+        resp = self.app.put('/v1.0/iam/put-user-policy',
+                            query_string={'account': self.account_id,
+                                          'user': self.user1,
+                                          'policy-name': 'mypolicy'},
+                            data=IAM_POLICY_FULLACCESS.encode('utf-8'))
+        self.assertEqual(resp.status_code, 201)
+        resp = self.app.put('/v1.0/iam/put-user-policy',
+                            query_string={'account': self.account_id,
+                                          'user': self.user1,
+                                          'policy-name': 'mysecondpolicy'},
+                            data=IAM_POLICY_FULLACCESS.encode('utf-8'))
+        self.assertEqual(resp.status_code, 201)
+        resp = self.app.get('/v1.0/iam/list-user-policies',
+                            query_string={'account': self.account_id,
+                                          'user': self.user1})
+        self.assertEqual(resp.status_code, 200)
+        actual = json.loads(resp.data.decode('utf-8'))
+        self.assertIn('PolicyNames', actual)
+        self.assertEqual(actual['PolicyNames'], ['mypolicy', 'mysecondpolicy'])
+
+        # Delete the policies
+        resp = self.app.delete('/v1.0/iam/delete-user-policy',
+                               query_string={'account': self.account_id,
+                                             'user': self.user1,
+                                             'policy-name': 'mypolicy'})
+        self.assertEqual(resp.status_code, 204)
+        resp = self.app.get('/v1.0/iam/list-user-policies',
+                            query_string={'account': self.account_id,
+                                          'user': self.user1})
+        self.assertEqual(resp.status_code, 200)
+        actual = json.loads(resp.data.decode('utf-8'))
+        self.assertIn('PolicyNames', actual)
+        self.assertEqual(actual['PolicyNames'], ['mysecondpolicy'])
+        resp = self.app.delete('/v1.0/iam/delete-user-policy',
+                               query_string={'account': self.account_id,
+                                             'user': self.user1,
+                                             'policy-name': 'mysecondpolicy'})
+        self.assertEqual(resp.status_code, 204)
+        resp = self.app.get('/v1.0/iam/list-user-policies',
+                            query_string={'account': self.account_id,
+                                          'user': self.user1})
+        self.assertEqual(resp.status_code, 200)
+        actual = json.loads(resp.data.decode('utf-8'))
+        self.assertIn('PolicyNames', actual)
+        self.assertFalse(actual['PolicyNames'])
+
+    def test_delete_user_policy_not_existing(self):
+        resp = self.app.delete('/v1.0/iam/delete-user-policy',
+                               query_string={'account': self.account_id,
+                                             'user': self.user1,
+                                             'policy-name': 'mypolicy'})
+        self.assertEqual(resp.status_code, 204)
