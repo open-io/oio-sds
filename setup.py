0fa9bdb8651854bea6af60cce22eb17ca90b35fd
Aymeric Ducroquetz <aymeric.ducroquetz@ovhcloud.com>
2021-05-06 10:47:41 +0200

    Enforce Python version to >=3.6.0

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 
 setuptools.setup(
+    python_requires='>=3.6.0',
     setup_requires=['pbr'],
     install_requires=[
         'six>=1.11.0',
@@ -10,7 +11,6 @@ setuptools.setup(
         "License :: OSI Approved :: "
         "GNU Lesser General Public License v3 or later (LGPLv3+)",
         "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
     ],
     pbr=True
