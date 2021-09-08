78114d305752aa830f456133e730756e784321d1
Aymeric Ducroquetz <aymeric.ducroquetz@ovhcloud.com>
2021-09-08 11:05:30 +0200

    event-agent: Allow to define connection timeout when deleting chunks

--- oio/event/filters/content_cleaner.py
+++ oio/event/filters/content_cleaner.py
@@ -16,12 +16,13 @@
 
 from oio.blob.client import BlobClient
 from oio.common.constants import REQID_HEADER
-from oio.event.evob import Event, EventTypes
-from oio.event.filters.base import Filter
+from oio.common.easy_value import int_value, float_value
 from oio.common.exceptions import OioException
-from oio.common.http_urllib3 import URLLIB3_POOLMANAGER_KWARGS
+from oio.common.http_urllib3 import URLLIB3_POOLMANAGER_KWARGS, urllib3
 from oio.common.storage_method import STORAGE_METHODS, guess_storage_method
 from oio.common.utils import request_id
+from oio.event.evob import Event, EventTypes
+from oio.event.filters.base import Filter
 
 
 class ContentReaperFilter(Filter):
@@ -37,8 +38,15 @@ class ContentReaperFilter(Filter):
         self.blob_client = BlobClient(self.conf, logger=self.logger,
                                       watchdog=self.app_env['watchdog'],
                                       **kwargs)
-        self.chunk_concurrency = int(self.conf.get('concurrency', 3))
-        self.chunk_timeout = float(self.conf.get('timeout', 5.0))
+        self.chunk_concurrency = int_value(self.conf.get('concurrency'), 3)
+        self.chunk_timeout = float_value(self.conf.get('timeout'), None)
+        if not self.chunk_timeout:
+            connection_timeout = float_value(
+                self.conf.get('connection_timeout'), 1.0)
+            read_timeout = float_value(self.conf.get('read_timeout'), 5.0)
+            self.chunk_timeout = urllib3.Timeout(
+                    connect=connection_timeout,
+                    read=read_timeout)
 
     def _handle_rawx(self, url, chunks, content_headers,
                      storage_method, reqid):
