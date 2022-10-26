326d2d79907d94b29f5f5381e555c3e4d101a232
Lhoussain Ait Assou <lhoussain.ait-assou@ovhcloud.com>
2022-10-27 01:22:34 +0200

    meta2 views: prepare view creation in meta2 db

--- proxy/metacd_http.c
+++ proxy/metacd_http.c
@@ -1188,6 +1188,7 @@ configure_request_handlers (void)
 	SET("/$NS/container/checkpoint/#POST", action_container_checkpoint);
 
 	// Lifecycle
+	SET("/$NS/container/lifecycle/views/create/#POST", action_container_lifecycle_create_views);
 	SET("/$NS/container/lifecycle/apply/#POST", action_container_lifecycle_apply);
 
 	// Sharding
