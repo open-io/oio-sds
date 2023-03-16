3a26b7593203be6c6f96f9c79818104ba1082bae
Florent Vennetier <florent.vennetier@ovhcloud.com>
2023-03-16 11:38:36 +0100

    Python API: ignore "reentrant call" message during socket close

--- oio/common/http_eventlet.py
+++ oio/common/http_eventlet.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2017-2020 OpenIO SAS, as part of OpenIO SDS
-# Copyright (C) 2021-2022 OVH SAS
+# Copyright (C) 2021-2023 OVH SAS
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -47,7 +47,11 @@ class CustomHTTPResponse(HTTPResponse):
 
     def close(self):
         if not self.isclosed():
-            super().close()
+            try:
+                super().close()
+            except RuntimeError as rerr:
+                if "reentrant call" not in str(rerr):
+                    raise
         if self.sock:
             try:
                 # Prevent long CLOSE_WAIT state
