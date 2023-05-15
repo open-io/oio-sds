a0591a8b188040bed3d21ad013f272e31040bec8
Florent Vennetier <florent.vennetier@ovhcloud.com>
2023-05-15 18:19:41 +0200

    Implement script to move messages from beanstalkd to RabbitMQ

--- tests/functional/event/test_events_emission.py
+++ tests/functional/event/test_events_emission.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Copyright (C) 2018-2019 OpenIO SAS, as part of OpenIO SDS
-# Copyright (C) 2022 OVH SAS
+# Copyright (C) 2022-2023 OVH SAS
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -16,11 +16,16 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library.
 
+import shutil
+import subprocess
 import time
-from oio.api.object_storage import ObjectStorageApi
+
+from signal import SIGINT
+
 from oio.event.evob import EventTypes
+from oio.common.amqp import AmqpConnector, AMQPError
+from oio.common.json import json
 from oio.common.utils import cid_from_name, request_id
-from oio.container.client import ContainerClient
 from tests.utils import BaseTestCase
 
 
@@ -29,17 +34,16 @@ REASONABLE_EVENT_DELAY = 3.0
 
 class TestMeta2EventsEmission(BaseTestCase):
     def setUp(self):
-        super(TestMeta2EventsEmission, self).setUp()
+        super().setUp()
         self.container_name = "TestEventsEmission%f" % time.time()
         self.container_id = cid_from_name(self.account, self.container_name)
-        self.container_client = ContainerClient(self.conf)
-        self.storage_api = ObjectStorageApi(self.conf["namespace"])
+        self.container_client = self.storage.container
         self.beanstalkd0.drain_tube("oio-preserved")
 
     def wait_for_all_events(self, types, reqid=None):
-        pulled_events = dict()
+        pulled_events = {}
         for event_type in types:
-            pulled_events[event_type] = list()
+            pulled_events[event_type] = []
 
         while True:
             event = self.wait_for_event(
@@ -86,7 +90,7 @@ class TestMeta2EventsEmission(BaseTestCase):
             )
 
         # Get the peers list and verify it's the same as received
-        raw_dir_info = self.storage_api.directory.list(
+        raw_dir_info = self.storage.directory.list(
             self.account, self.container_name, cid=self.container_id
         )
         raw_dir_info = raw_dir_info["srv"]
@@ -107,7 +111,7 @@ class TestMeta2EventsEmission(BaseTestCase):
         self.container_client.container_create(self.account, self.container_name)
 
         # Get the peers list
-        raw_dir_info = self.storage_api.directory.list(
+        raw_dir_info = self.storage.directory.list(
             self.account, self.container_name, cid=self.container_id
         )
         raw_dir_info = raw_dir_info["srv"]
@@ -148,3 +152,96 @@ class TestMeta2EventsEmission(BaseTestCase):
             [event.data.get("peer") for event in meta2_deleted_events]
         )
         self.assertListEqual(expected_peers_list, received_peers)
+
+
+class TestEventRouting(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.container_name = "TestEventrouting%f" % time.time()
+        self.beanstalkd0.drain_tube("oio-preserved")
+
+    def test_beanstalkd_to_rabbitmq(self):
+        """
+        Test the forwarding of events from Beanstalkd to RabbitMQ.
+        """
+        # XXX: "event-agent.meta2" has a query string, and we don't want it.
+        # And "event-agent" is a Beanstalkd endpoint.
+        rab_endpoint = self.ns_conf.get("event-agent.rawx")
+        if not rab_endpoint or not rab_endpoint.startswith("amqp://"):
+            self.skipTest("This test requires a RabbitMQ event-agent")
+        cmdline = [
+            "coverage",
+            "run",
+            "-p",
+            "--context",
+            "event-agent",
+            shutil.which("oio-beanstalkd-to-rabbitmq"),
+            "--ns",
+            self.ns,
+            "--src-tube",
+            "oio-preserved",
+            "--src-endpoint",
+            self.conf["main_queue_url"],
+            "--dst-endpoint",
+            rab_endpoint,
+            "--dst-exchange",
+            "oio-preserved",
+            "--dst-queue",
+            "oio-preserved",
+        ]
+        # Start a subprocess which will read events from "oio-preserved"
+        # Beanstalkd tube and write them in "oio-preserved" RabbitMQ queue.
+        self.logger.info("Starting subprocess: %s", " ".join(cmdline))
+        child = subprocess.Popen(
+            cmdline,
+            close_fds=True,
+        )
+        # Produce some events
+        obj_name = "myobject"
+        reqid0 = request_id("btor-")
+        self.storage.object_create(
+            self.account,
+            self.container_name,
+            obj_name=obj_name,
+            data=obj_name.encode("utf-8"),
+            reqid=reqid0,
+        )
+        self.storage.object_delete(
+            self.account, self.container_name, obj_name, reqid=reqid0
+        )
+
+        try:
+            rab = AmqpConnector(endpoints=rab_endpoint, logger=self.logger)
+            # Wait for the queue to be created by the subprocess
+            for _ in range(5):
+                try:
+                    rab.maybe_reconnect()
+                    rab._channel.queue_declare(
+                        "oio-preserved",
+                        durable=True,
+                        passive=True,
+                    )
+                    break
+                except AMQPError as err:
+                    self.logger.warn("Queue not declared yet: %s", err)
+                    time.sleep(0.5)
+
+            # Now check the events have been forwarded
+            del_event_received = False
+            for method_frame, properties, body in rab._channel.consume(
+                "oio-preserved", inactivity_timeout=1
+            ):
+                if (method_frame, properties, body) == (None, None, None):
+                    break
+                decoded = json.loads(body)
+                reqid = decoded.get("request_id")
+                event_type = decoded.get("event")
+                if reqid == reqid0:
+                    if event_type == EventTypes.CHUNK_DELETED:
+                        del_event_received = True
+                    rab._channel.basic_ack(method_frame.delivery_tag)
+
+            self.assertTrue(del_event_received)
+        finally:
+            child.send_signal(SIGINT)
+            child.wait(3)
