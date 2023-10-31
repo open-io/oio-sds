7e18f0ef1fb04bb5d72e7fceb83fb271602f2c07
Loic Metzger <loic.metzger@ovhcloud.com>
2023-10-31 11:25:53 +0100

    encryption tool: add crypto body metadata
    Create bucket secret in kms. This secret is used as the object key.
    The body_key is wrapped using the object key and a randomly chosen IV:
       wrapped_body_key = E(body_key, object_key, body_key_iv)
    The encrypter stores the associated crypto-metadata in a system metadata
    header:
       X-Object-Sysmeta-Crypto-Body-Meta:
           {"iv": body_iv,
            "cipher": "AES_CTR_256",
            "body_key": {"key": wrapped_body_key,
                         "iv": body_key_iv}}

--- tools/encrypter.py
+++ tools/encrypter.py
@@ -2,11 +2,42 @@
 
 # Copyright (C) 2024 OVH SAS
 
+import argparse
+import json
 import sys
 from encryption import Encrypter
 
+parser = argparse.ArgumentParser(description="encryption tool")
+parser.add_argument(
+    "--metadata",
+    type=str,
+    default="metadata.json",
+    help="path of object metadata json file",
+)
+parser.add_argument(
+    "--account",
+    type=str,
+    default="AUTH_demo",
+    help="name of the account in which the object is stored",
+)
+parser.add_argument(
+    "--container",
+    type=str,
+    required=True,
+    help="name of the container in which the object is stored",
+)
+parser.add_argument(
+    "--obj", type=str, required=True, help="name of the object to fetch"
+)
+args = parser.parse_args()
+
+
+# Read metadata
+with open(args.metadata, "r") as infile:
+    metadata = json.load(infile)
+
 # Encrypter object
-encrypter = Encrypter()
+encrypter = Encrypter(account=args.account, container=args.container, obj=args.obj)
 
 while 1:
     chunk = sys.stdin.buffer.read()
@@ -14,3 +45,7 @@ while 1:
         break
     ciphertext = encrypter.encrypt(chunk)
     sys.stdout.buffer.write(ciphertext)
+
+new_metadata = encrypter.encrypt_metadata(metadata)
+with open("metadata.json", "w") as outfile:
+    json.dump(new_metadata, outfile)
