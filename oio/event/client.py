6cba83b5431716f96c7c72e46f92bb24399a9872
Florent Vennetier <florent.vennetier@ovhcloud.com>
2024-03-08 11:50:03 +0100

    Fix "openio event" CLI when Kafka is configured in place of beanstalkd

--- oio/event/client.py
+++ oio/event/client.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2015-2020 OpenIO SAS, as part of OpenIO SDS
-# Copyright (C) 2021 OVH SAS
+# Copyright (C) 2021-2024 OVH SAS
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -23,7 +23,18 @@ from oio.event.beanstalk import Beanstalk
 class EventClient(object):
     def __init__(self, conf, **kwargs):
         self.ns_conf = load_namespace_conf(conf["namespace"])
-        self.queue_url = self.ns_conf["event-agent"].split(";")
+        for queue_url in (
+            self.ns_conf["event-agent"],
+            self.ns_conf.get("beanstalk", ""),
+        ):
+            if queue_url.startswith("beanstalk"):
+                self.queue_url = queue_url.split(";")
+                break
+        else:
+            raise ValueError(
+                "The configured event-agent is not using beanstalkd, please "
+                f"use another tool adapted to {self.ns_conf['event-agent']}"
+            )
         self._beanstalk = None
 
     @property
