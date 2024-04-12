6a34632b55e18e542ba9d81de242c34909c438eb
Florent Vennetier <florent.vennetier@ovhcloud.com>
2024-04-12 10:42:41 +0200

    CI: remove beanstalkd and kafka options
    Kafka is the default now, we don't need an option.

--- tools/oio-test-suites.sh
+++ tools/oio-test-suites.sh
@@ -348,7 +348,6 @@ if is_running_test_suite "single" ; then
 	fi
 	func_tests $opts \
 		-f "${SRCDIR}/etc/bootstrap-preset-SINGLE.yml" \
-		-f "${SRCDIR}/etc/bootstrap-option-kafka.yml" \
 		-f "${SRCDIR}/etc/bootstrap-option-udp.yml" \
 		-f "${SRCDIR}/etc/bootstrap-option-long-timeouts.yml" \
 		-f "${SRCDIR}/etc/bootstrap-meta1-1digits.yml"
@@ -357,7 +356,6 @@ fi
 if is_running_test_suite "repli" ; then
 	echo -e "\n### Replication tests"
 	func_tests -f "${SRCDIR}/etc/bootstrap-preset-fullrepli.yml" \
-		-f "${SRCDIR}/etc/bootstrap-option-kafka.yml" \
 		-f "${SRCDIR}/etc/bootstrap-option-udp.yml" \
 		-f "${SRCDIR}/etc/bootstrap-option-long-timeouts.yml" \
 		-f "${SRCDIR}/etc/bootstrap-meta1-1digits.yml"
@@ -369,7 +367,6 @@ if is_running_test_suite "worm" ; then
 	for nb in 0 1 2 3 ; do
 		test_meta2_filters -f "${SRCDIR}/etc/bootstrap-preset-SINGLE.yml" \
 			-f "${SRCDIR}/etc/bootstrap-option-worm.yml" \
-			-f "${SRCDIR}/etc/bootstrap-option-kafka.yml" \
 			-f "${SRCDIR}/etc/bootstrap-meta1-${nb}digits.yml"
 	done
 	unset WORM
@@ -381,7 +378,6 @@ if is_running_test_suite "slave" ; then
 	for nb in 0 1 2 3 ; do
 		test_meta2_filters -f "${SRCDIR}/etc/bootstrap-preset-SINGLE.yml" \
 			-f "${SRCDIR}/etc/bootstrap-option-slave.yml" \
-			-f "${SRCDIR}/etc/bootstrap-option-kafka.yml" \
 			-f "${SRCDIR}/etc/bootstrap-meta1-${nb}digits.yml"
 	done
 	unset SLAVE
@@ -390,14 +386,12 @@ fi
 if is_running_test_suite "cli" ; then
 	echo -e "\n### CLI tests"
 	test_cli -f "${SRCDIR}/etc/bootstrap-preset-SINGLE.yml" \
-		-f "${SRCDIR}/etc/bootstrap-option-kafka.yml" \
 		-f "${SRCDIR}/etc/bootstrap-option-cache.yml"
 fi
 
 if is_running_test_suite "3copies" ; then
 	echo -e "\n### 3copies tests"
 	func_tests -f "${SRCDIR}/etc/bootstrap-preset-3COPIES-11RAWX.yml" \
-		-f "${SRCDIR}/etc/bootstrap-option-kafka.yml" \
 		-f "${SRCDIR}/etc/bootstrap-option-3hosts.yml"
 fi
 
@@ -405,17 +399,8 @@ if is_running_test_suite "ec" ; then
 	echo -e "\n### EC tests"
 
 	func_tests -f "${SRCDIR}/etc/bootstrap-preset-ANY-E93.yml" \
-		-f "${SRCDIR}/etc/bootstrap-option-kafka.yml" \
 		-f "${SRCDIR}/etc/bootstrap-option-4hosts.yml" \
 		-f "${SRCDIR}/etc/bootstrap-option-cache.yml"
-fi
-
-if is_running_test_suite "multi-beanstalk" ; then
-	echo -e "\n### Tests with multiple beanstalkd"
-	func_tests \
-		-f "${SRCDIR}/etc/bootstrap-preset-SINGLE.yml" \
-		-f "${SRCDIR}/etc/bootstrap-option-kafka.yml" \
-		-f "${SRCDIR}/etc/bootstrap-option-3beanstalkd.yml"
 fi
 
 func_tests_rebuilder_mover () {
@@ -484,12 +469,9 @@ if is_running_test_suite "rebuilder" ; then
 		-f "${SRCDIR}/etc/bootstrap-preset-fullrepli.yml" \
 		-f "${SRCDIR}/etc/bootstrap-option-udp.yml" \
 		-f "${SRCDIR}/etc/bootstrap-option-long-timeouts.yml" \
-		-f "${SRCDIR}/etc/bootstrap-option-kafka.yml" \
-		-f "${SRCDIR}/etc/bootstrap-meta1-1digits.yml" \
-		-f "${SRCDIR}/etc/bootstrap-option-3beanstalkd.yml"
+		-f "${SRCDIR}/etc/bootstrap-meta1-1digits.yml"
 
 	func_tests_rebuilder_mover \
-		-f "${SRCDIR}/etc/bootstrap-option-kafka.yml" \
 		-f "${SRCDIR}/etc/bootstrap-preset-EC.yml"
 
 	unset REBUILDER
@@ -503,7 +485,6 @@ if is_running_test_suite "mover" ; then
 	func_tests_rebuilder_mover \
 		-f "${SRCDIR}/etc/bootstrap-preset-smallrepli.yml" \
 		-f "${SRCDIR}/etc/bootstrap-option-udp.yml" \
-		-f "${SRCDIR}/etc/bootstrap-option-kafka.yml" \
 		-f "${SRCDIR}/etc/bootstrap-option-long-timeouts.yml" \
 		-f "${SRCDIR}/etc/bootstrap-meta1-1digits.yml"
 
