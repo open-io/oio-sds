6394b45159164fb0b9c1acda6b6c91208bb4c826
Florent Vennetier <florent.vennetier@ovhcloud.com>
2025-01-10 14:32:10 +0100

    Fix some copyrights

--- proxy/common.c
+++ proxy/common.c
@@ -1,7 +1,7 @@
 /*
 OpenIO SDS proxy
 Copyright (C) 2015-2019 OpenIO SAS, as part of OpenIO SDS
-Copyright (C) 2021-2024 OVH SAS
+Copyright (C) 2021-2025 OVH SAS
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as
