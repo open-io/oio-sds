0a9cbb1b17990501c498ab49ca672fcae5fb8af8
lhoussain.ait-assou <lhoussain.ait-assou@ovhcloud.com>
2025-01-22 10:03:03 +0100

    meta2: add meta2 code to handle allowed transitions

--- metautils/lib/storage_policy.h
+++ metautils/lib/storage_policy.h
@@ -2,7 +2,7 @@
 OpenIO SDS metautils
 Copyright (C) 2014 Worldline, as part of Redcurrant
 Copyright (C) 2015-2020 OpenIO SAS, as part of OpenIO SDS
-Copyright (C) 2020 OVH SAS
+Copyright (C) 2020-2025 OVH SAS
 
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
@@ -116,5 +116,7 @@ data_security_decode_param_int64 (const char *encoded, const char *k, gint64 def
 	g_free(prefix);
 	return rc;
 }
+
+gchar * split_policy(const char * current_value);
 
 #endif /*OIO_SDS__metautils__lib__storage_policy_h*/
