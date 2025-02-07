f975608b0afd9b66e455ba0c079df20f74c442f4
Jean-Francois Smigielski <jean-francois.smigielski@ovhcloud.com>
2025-02-07 11:57:19 +0100

    rawx: Fix the copyright preambles

--- rawx/handler_chunk.go
+++ rawx/handler_chunk.go
@@ -1,6 +1,6 @@
 // OpenIO SDS Go rawx
 // Copyright (C) 2015-2020 OpenIO SAS
-// Copyright (C) 2021-2024 OVH SAS
+// Copyright (C) 2021-2025 OVH SAS
 //
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Affero General Public
