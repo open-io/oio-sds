4cfe63580aad2c7956b869c228189a8cec10c6fe
aitassou <lhoussain.ait-assou@ovhcloud.com>
2025-11-26 10:31:24 +0100

    cli: remove unused lifecycle status command

--- oio/cli/lifecycle/lifecycle.py
+++ oio/cli/lifecycle/lifecycle.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2017-2020 OpenIO SAS, as part of OpenIO SDS
-# Copyright (C) 2022-2024 OVH SAS
+# Copyright (C) 2022-2025 OVH SAS
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -16,13 +16,12 @@
 
 """Lifecycle-related commands"""
 
-from collections import defaultdict
 from logging import getLogger
 
 from oio.cli import Command, Lister
 from oio.common.exceptions import LifecycleNotFound
 from oio.container.lifecycle import ContainerLifecycle
-from oio.lifecycle.metrics import LifecycleMetricTracker, LifecycleStep
+from oio.lifecycle.metrics import LifecycleMetricTracker
 
 
 class LifecycleSet(Command):
@@ -94,95 +93,6 @@ class LifecycleGet(Command):
                 f" in account {self.app.client_manager.account}"
             )
         self.app.stdout.write(json_conf)
-
-
-class LifecycleStatus(Lister):
-    log = getLogger(__name__ + ".LifecycleStatus")
-
-    CONF_KEY_PREFIX = "lifecycle."
-
-    def get_parser(self, prog_name):
-        parser = super().get_parser(prog_name)
-
-        parser.add_argument(
-            "run_id",
-            metavar="<run_id>",
-            help="Lifecycle run id",
-        )
-        parser.add_argument(
-            "bucket",
-            metavar="<bucket_id>",
-            help="Bucket id",
-        )
-        parser.add_argument(
-            "--container",
-            metavar="<container>",
-            help="Container id for detailed progression",
-        )
-
-        return parser
-
-    def __metrics_format(self, metrics):
-        d = defaultdict(dict)
-        for step in LifecycleStep:
-            for k, v in metrics.get(step, {}).items():
-                d[k][step] = v
-
-        counters = []
-        for k, v in d.items():
-            processed = int(v.get(LifecycleStep.PROCESSED, 0))
-            submitted = int(v.get(LifecycleStep.SUBMITTED, 0))
-            skipped = int(v.get(LifecycleStep.SKIPPED, 0))
-            progress = None
-            if submitted:
-                progress = (processed + skipped) / submitted
-                progress = f"{progress:.0%}"
-            counters.append((k, None, submitted, skipped, processed, progress))
-
-        return counters
-
-    def take_action(self, parsed_args):
-        account = self.app.client_manager.account
-
-        conf = {
-            k[len(self.CONF_KEY_PREFIX) :]: v
-            for k, v in self.app.client_manager.sds_conf.items()
-            if k.startswith(self.CONF_KEY_PREFIX)
-        }
-
-        client = LifecycleMetricTracker(conf, logger=self.log)
-
-        if parsed_args.container:
-            metrics = client.get_container_metrics(
-                parsed_args.run_id,
-                account,
-                parsed_args.bucket,
-                parsed_args.container,
-            )
-        else:
-            metrics = client.get_bucket_metrics(
-                parsed_args.run_id, account, parsed_args.bucket
-            )
-
-        counters = self.__metrics_format(metrics)
-        global_counters = [
-            ("objects", metrics.get("objects", 0), None, None, None, None)
-        ]
-
-        return (
-            [
-                "metrics",
-                "",
-                LifecycleStep.SUBMITTED.value,
-                LifecycleStep.SKIPPED.value,
-                LifecycleStep.PROCESSED.value,
-                "progression",
-            ],
-            [
-                ["n/a" if v is None else v for v in c]
-                for c in (*global_counters, *counters)
-            ],
-        )
 
 
 class LifecycleContainers(Lister):
