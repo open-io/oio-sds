3b9bdd05fcf9703a0e5f1fb753102e9c024fd98e
Franco Davy Irakoze <franco-davy.irakoze@ovhcloud.com>
2026-01-06 16:58:26 +0100

    oio: fix some copyright

--- oio/crawler/meta2/filters/auto_vacuum.py
+++ oio/crawler/meta2/filters/auto_vacuum.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2021-2025 OVH SAS
+# Copyright (C) 2021-2026 OVH SAS
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
--- tests/functional/container/test_sharding.py
+++ tests/functional/container/test_sharding.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2021-2025 OVH SAS
+# Copyright (C) 2021-2026 OVH SAS
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
