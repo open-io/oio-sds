1fc79722a146cba3baf3bc8f03612cba18ea83f0
Aymeric Ducroquetz <aymeric.ducroquetz@ovhcloud.com>
2026-01-08 17:15:20 +0100

    oio: Fix some copyright

--- tests/functional/event/filters/test_deadletter_consumer.py
+++ tests/functional/event/filters/test_deadletter_consumer.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2025 OVH SAS
+# Copyright (C) 2025-2026 OVH SAS
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
